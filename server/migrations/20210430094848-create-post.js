984267a4a44fba3ad29649242bcc66dba4260819
Shreedhar Acharya <spacharya2537@gmail.com>
2021-04-30 15:35:35 +0545

    migration and modal for post

---
+++ server/migrations/20210430094848-create-post.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      post_name_nep: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      post_name_eng: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('posts');
+  }
+};
