7c636f0dd16f03cf1a2a5161d884bf2c0e7c71f4
Shreedhar Acharya <spacharya2537@gmail.com>
2021-05-10 16:01:44 +0545

    [FM-29]Controller for CRUD Post operations

---
+++ server/app/controller/postController.js
@@ -0,0 +1,72 @@
+const db = require("../db");
+const {
+  getAllPost,
+  getPost,
+  addPost,
+  updatePost,
+  deletePost,
+} = require("../services/postServices");
+
+module.exports = {
+  getAllPost: (req, res) => {
+    getAllPost((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("results", results);
+      return res.json(results);
+    });
+  },
+};
+
+module.exports = {
+  getPost: (req, res) => {
+    getPost((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("results", results);
+      return res.json(results);
+    });
+  },
+};
+
+module.exports = {
+  addPost: (req, res) => {
+    addPost((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("results", results);
+      return res.json(results);
+    });
+  },
+};
+
+module.exports = {
+  updatePost: (req, res) => {
+    updatePost((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("results", results);
+      return res.json(results);
+    });
+  },
+};
+module.exports = {
+  deletePost: (req, res) => {
+    deletePost((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("results", results);
+      return res.json(results);
+    });
+  },
+};
