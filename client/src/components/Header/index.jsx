0efd5edd9b74332c64fb1b7a7ffb8373552432da
Shreedhar Acharya <spacharya2537@gmail.com>
2021-05-11 17:55:25 +0545

    [FM-36]updating header component

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -4,19 +4,28 @@ import "./Header.scss";
 
 class HeaderComponent extends Component {
   render() {
+    const { enabled } = this.props;
     return (
-      <Row className="header_content">
-        <Col xs={12} sm={6} md={3} lg={3} className="app-logo">
-          <Image src="/images/nepal_logon.png" />
-        </Col>
-        <Col xs={12} sm={6} md={3} lg={3} className="info_section">
-          <p>प्रदेश सरकार</p>
-          <p>उद्योग, पर्यटन, वन तथा वातावरण मन्त्रालय</p>
-          <p>गण्डकी प्रदेश, पोखरा</p>
-        </Col>
-        <Col xs={12} sm={6} md={3} lg={3} className="app-logo">
-          <Image src="/nepalflag.gif" />
-        </Col>
+      <Row className={enabled ? "" : "m-0"}>
+        {enabled ? (
+          <div className="header_content">
+            <div className="gov-logo">
+              <Image src="/images/nepal-govt.png" className="img-fluid" />
+            </div>
+            <div className="text-center">
+              <p>प्रदेश सरकार</p>
+              <p>उद्योग, पर्यटन, वन तथा वातावरण मन्त्रालय</p>
+              <p>गण्डकी प्रदेश, पोखरा</p>
+            </div>
+            <div>
+              <Image src="/nepalflag.gif" className="img-fluid flag" />
+            </div>
+          </div>
+        ) : (
+          <div className="gov-logo-collapsed">
+            <Image src="/images/nepal-govt.png" className="img-height" />
+          </div>
+        )}
       </Row>
     );
   }
