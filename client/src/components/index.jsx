9ea3c7c13fbf4a75d0bb9e68391db6191b2b3793
Shreedhar Acharya <spacharya2537@gmail.com>
2021-05-11 17:53:52 +0545

    [FM-36] importing and exporting sideNavbar component

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,5 +1,6 @@
 import HeaderComponent from "./Header";
 import NavbarComponent from "./Navbar";
 import NotFound from "./NotFound";
+import SideNavbar from "./SideNavbar";
 
-export { HeaderComponent, NavbarComponent, NotFound };
+export { HeaderComponent, NavbarComponent, NotFound, SideNavbar };
