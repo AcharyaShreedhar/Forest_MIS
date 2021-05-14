3c47d085a336767e2fce2cdb8bb8ace45c358be6
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-05-14 16:43:05 +0545

    [FM-53]controller update3 for nijiban

--- server/app/routes/nijiBanBibaranroutes.js
+++ server/app/routes/nijiBanBibaranroutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const NijibanBibaranController = require("../controller/NijibanBibaranController");
-router.get("/province", NijibanBibaranController.getAllNijibanBibaran);
-router.get("/province/provinceId", NijibanBibaranController.getNijibanBibaran);
-router.post("/province", NijibanBibaranController.addNijibanBibaran);
-router.put("/province", NijibanBibaranController.updateNijibanBibaran);
-router.delete("/province", NijibanBibaranController.deleteNijibanBibaran);
+const nijibanBibaranController = require("../controller/nijibanBibaranController");
+router.get("/province", nijibanBibaranController.getAllnijibanBibaran);
+router.get("/province/provinceId", nijibanBibaranController.getnijibanBibaran);
+router.post("/province", nijibanBibaranController.addnijibanBibaran);
+router.put("/province", nijibanBibaranController.updatenijibanBibaran);
+router.delete("/province", nijibanBibaranController.deletenijibanBibaran);
 module.exports = router;
