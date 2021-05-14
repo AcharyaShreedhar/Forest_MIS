4daa2949a33198939475272f55313de8c08c64a7
Shreedhar Acharya <spacharya2537@gmail.com>
2021-05-14 13:16:55 +0545

    [FM-38]model for assets

---
+++ server/models/assets.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class assets extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  assets.init({
+    asset_type: DataTypes.STRING,
+    asset_loc: DataTypes.STRING,
+    kitta_no: DataTypes.STRING,
+    home_area: DataTypes.STRING,
+    land_area: DataTypes.STRING,
+    unit: DataTypes.STRING,
+    remarks: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'assets',
+  });
+  return assets;
+};
