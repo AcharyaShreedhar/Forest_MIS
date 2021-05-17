4dea9925f91dc437d5867e16539fde4a5816a475
Shreedhar Acharya <spacharya2537@gmail.com>
2021-05-17 14:19:08 +0545

    [FM-42]Model for Niji Ban Bibaran

---
+++ server/models/nijiban_bibaran.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class nijiban_bibaran extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  nijiban_bibaran.init({
+    swikrit_miti: DataTypes.STRING,
+    nijiban_dhaniko_naam: DataTypes.STRING,
+    perm_addr: DataTypes.STRING,
+    curr_addr: DataTypes.STRING,
+    area: DataTypes.STRING,
+    main_species: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'nijiban_bibaran',
+  });
+  return nijiban_bibaran;
+};
