5fc0872c6fc22cc8ac3ae1113a19ea638be1d0b9
Shreedhar Acharya <spacharya2537@gmail.com>
2021-05-18 16:51:06 +0545

    [FM-48]Model Karmarchari Darbandi

---
+++ server/models/karmachari_darbandi.js
@@ -0,0 +1,27 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class karmachari_darbandi extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  karmachari_darbandi.init(
+    {
+      post: DataTypes.STRING,
+      kayam_darbandi_sankhya: DataTypes.STRING,
+      padpurti_sankhya: DataTypes.STRING,
+      khali_sankhya: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "karmachari_darbandi",
+    }
+  );
+  return karmachari_darbandi;
+};
