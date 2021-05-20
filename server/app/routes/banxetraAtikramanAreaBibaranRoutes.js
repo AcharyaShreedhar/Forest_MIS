08525e07a80a84ebbf9ef538385110b832e760b1
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-05-20 18:33:28 +0545

    [FM-64]routes update1  bannxetraAtikramanAreaBibaran

--- server/app/routes/banxetraAtikramanAreaBibaranRoutes.js
+++ server/app/routes/banxetraAtikramanAreaBibaranRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const banxetraAtikramanAreaController = require("../controller/banxetraAtikramanAreaController");
-router.get("/banxetraAtikramanArea", banxetraAtikramanAreaController.getAllBanxetraAtikramanArea);
-router.get("/banxetraAtikramanArea/banxetraAtikramanAreaId", banxetraAtikramanAreaController.getBanxetraAtikramanArea);
-router.post("/banxetraAtikramanArea", banxetraAtikramanAreaController.addBanxetraAtikramanArea);
-router.put("/banxetraAtikramanArea", banxetraAtikramanAreaController.updateBanxetraAtikramanArea);
-router.delete("/banxetraAtikramanArea", banxetraAtikramanAreaController.deleteBanxetraAtikramanArea);
+const banxetraAtikramanAreaBibaranController = require("../controller/banxetraAtikramanAreaBibaranController");
+router.get("/banxetraAtikramanAreaBibaran", banxetraAtikramanAreaBibaranController.getAllBanxetraAtikramanAreaBibaran);
+router.get("/banxetraAtikramanAreaBibaran/banxetraAtikramanAreaBibaranId", banxetraAtikramanAreaBibaranController.getBanxetraAtikramanAreaBibaran);
+router.post("/banxetraAtikramanAreaBibaran", banxetraAtikramanAreaBibaranController.addBanxetraAtikramanAreaBibaran);
+router.put("/banxetraAtikramanAreaBibaran", banxetraAtikramanAreaBibaranController.updateBanxetraAtikramanAreaBibaran);
+router.delete("/banxetraAtikramanAreaBibaran", banxetraAtikramanAreaBibaranController.deleteBanxetraAtikramanAreaBibaran);
 module.exports = router;
