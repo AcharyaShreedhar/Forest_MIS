1e7839ac1b97003fa354b51533fcc8e99e1e9cba
Shreedhar Acharya <spacharya2537@gmail.com>
2021-05-21 18:38:53 +0545

    [FM-81]Model for Karyalaya ko karmachari darbandi abam padpurti bibaran'

---
+++ server/models/office_staff_post.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class office_staff_post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  office_staff_post.init({
+    post: DataTypes.STRING,
+    kayam_darbandi_sankhya: DataTypes.STRING,
+    padpurti_sankhya: DataTypes.STRING,
+    khali_sankhya: DataTypes.STRING,
+    created_by: DataTypes.STRING,
+    updated_by: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'office_staff_post',
+  });
+  return office_staff_post;
+};
