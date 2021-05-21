2ce7908bb7c2985d6839f29dcb60dd81cbf3736a
Shreedhar Acharya <spacharya2537@gmail.com>
2021-05-21 18:37:57 +0545

    [FM-81]Migration for Karyalaya ko karmachari darbandi abam padpurti bibaran

---
+++ server/migrations/20210521125141-create-office-staff-post.js
@@ -0,0 +1,42 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("office_staff_posts", {
+      office_staff_post_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      post: {
+        type: Sequelize.STRING,
+      },
+      kayam_darbandi_sankhya: {
+        type: Sequelize.STRING,
+      },
+      padpurti_sankhya: {
+        type: Sequelize.STRING,
+      },
+      khali_sankhya: {
+        type: Sequelize.STRING,
+      },
+      created_by: {
+        type: Sequelize.STRING,
+      },
+      updated_by: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("office_staff_posts");
+  },
+};
