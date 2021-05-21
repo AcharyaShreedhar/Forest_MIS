843ddc7d1e2fabeefa5884e3712f29da3516437e
Shreedhar Acharya <spacharya2537@gmail.com>
2021-05-21 15:34:18 +0545

    Model for Niji Ban/Samudayik aabadibata ban paidawar wosarpasarko lagi prabidhik sahayog

---
+++ server/models/ban_paidawar.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ban_paidawar extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  ban_paidawar.init({
+    ban_id: DataTypes.STRING,
+    arthik_barsa: DataTypes.STRING,
+    mahina: DataTypes.STRING,
+    kaath: DataTypes.STRING,
+    daura: DataTypes.STRING,
+    lavgrahi_sankhya: DataTypes.STRING,
+    mulyaabhibridi_kar: DataTypes.STRING,
+    created_by: DataTypes.STRING,
+    updated_by: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'ban_paidawar',
+  });
+  return ban_paidawar;
+};
