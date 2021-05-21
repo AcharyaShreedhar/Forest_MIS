897837f24de5b1e93b1cb9f23635a0921010d918
Shreedhar Acharya <spacharya2537@gmail.com>
2021-05-21 15:17:42 +0545

    [FM-68]Migration for Niji Ban/Samudayik aabadibata ban paidawar wosarpasarko lagi prabidhik sahayog

---
+++ server/migrations/20210521093158-create-ban-paidawar.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ban_paidawars', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ban_id: {
+        type: Sequelize.STRING
+      },
+      arthik_barsa: {
+        type: Sequelize.STRING
+      },
+      mahina: {
+        type: Sequelize.STRING
+      },
+      kaath: {
+        type: Sequelize.STRING
+      },
+      daura: {
+        type: Sequelize.STRING
+      },
+      lavgrahi_sankhya: {
+        type: Sequelize.STRING
+      },
+      mulyaabhibridi_kar: {
+        type: Sequelize.STRING
+      },
+      created_by: {
+        type: Sequelize.STRING
+      },
+      updated_by: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ban_paidawars');
+  }
+};
