08331a1497461aa9ad502702f5a7bba41ef2a9af
Shreedhar Acharya <spacharya2537@gmail.com>
2021-05-21 18:14:07 +0545

    [FM-76]Migration for Ban Dadelo Sambhandhi Bibaran

---
+++ server/migrations/20210521122806-create-bandadelo-bibaran.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('bandadelo_bibarans', {
+      bandadelo_bibaran_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      bandadelo_address: {
+        type: Sequelize.STRING
+      },
+      ban_type: {
+        type: Sequelize.STRING
+      },
+      ban_prajati: {
+        type: Sequelize.STRING
+      },
+      xeti_area: {
+        type: Sequelize.STRING
+      },
+      niyantran_karta: {
+        type: Sequelize.STRING
+      },
+      sahabhagi_mahila: {
+        type: Sequelize.STRING
+      },
+      sahabhagi_purus: {
+        type: Sequelize.STRING
+      },
+      created_by: {
+        type: Sequelize.STRING
+      },
+      updated_by: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('bandadelo_bibarans');
+  }
+};
