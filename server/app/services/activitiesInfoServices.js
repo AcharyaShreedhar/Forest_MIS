c2c68f781f3ef9dfaf35b4a6f56ef2b52adb9a13
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-05-23 13:48:20 +0545

    [FM-56]services update2  activitiesInfo

--- server/app/services/activitiesInfoServices.js
+++ server/app/services/activitiesInfoServices.js
@@ -20,7 +20,7 @@ module.exports = {
     const getActivitiesInfoQuery = `select * from activities_info where activities_info_id=$1`;
     pool.query(
       getActivitiesInfoQuery,
-      [req.params.ActivitiesInfoId],
+      [req.params.activitiesInfoId],
       (error, results, fields) => {
         if (error) {
           callBack(error);
@@ -51,7 +51,7 @@ module.exports = {
 //Service for updating a ActivitiesInfo
 module.exports = {
   updateActivitiesInfo: (callBack) => {
-    const updateActivitiesInfoQuery = `UPDATE ActivitiesInfo SET fiscal_year=$1, community_forest_name=$2, area=$3, production_from_conservation_timber=$4, production_from_conservation_wood=$5, employment_generated_workingday=$6, distribution_withingroup_timber=$7, distribution_withingroup_wood=$8, distribution_outsidegroup_timber=$9, distribution_outsidegroup_timber=$10, distribution_outsidegroup_wood=$11, distribution_maujdat_timber=$12, distribution_maujdat_wood=$13, amdani_kharcha_bibaran_annual_income=$14, amdani_kharcha_bibaran_annual_expenditure=$15, amdani_kharcha_bibaran_nett_annual_saving=$16, niyamit_rojgar_count=$17, community_udhyam_bibaran=$18, annual_bibaran_bujhayeko_nabujhayeko=$19, lekha_parikshyan_garaiyeko_nagariyeko=$20,created_by=$21, updated_by=$22 WHERE activitiesInfo_id=$23 returning *`;
+    const updateActivitiesInfoQuery = `UPDATE activities_info SET fiscal_year=$1, community_forest_name=$2, area=$3, production_from_conservation_timber=$4, production_from_conservation_wood=$5, employment_generated_workingday=$6, distribution_withingroup_timber=$7, distribution_withingroup_wood=$8, distribution_outsidegroup_timber=$9, distribution_outsidegroup_timber=$10, distribution_outsidegroup_wood=$11, distribution_maujdat_timber=$12, distribution_maujdat_wood=$13, amdani_kharcha_bibaran_annual_income=$14, amdani_kharcha_bibaran_annual_expenditure=$15, amdani_kharcha_bibaran_nett_annual_saving=$16, niyamit_rojgar_count=$17, community_udhyam_bibaran=$18, annual_bibaran_bujhayeko_nabujhayeko=$19, lekha_parikshyan_garaiyeko_nagariyeko=$20,created_by=$21, updated_by=$22 WHERE activitiesInfo_id=$23 returning *`;
     pool.query(
       updateActivitiesInfoQuery,
       [req.body.fiscal_year, req.body.community_forest_name, req.body.area, req.body.production_from_conservation_timber, req.body.production_from_conservation_wood, req.body.employment_generated_workingday, req.body.distribution_withingroup_timber, req.body.distribution_withingroup_wood, req.body.distribution_outsidegroup_timber, req.body.distribution_outsidegroup_timber, req.body.distribution_outsidegroup_wood, req.body.distribution_maujdat_timber, req.body.distribution_maujdat_wood, req.body.amdani_kharcha_bibaran_annual_income, req.body.amdani_kharcha_bibaran_annual_expenditure, req.body.amdani_kharcha_bibaran_nett_annual_saving, req.body.niyamit_rojgar_count, req.body.community_udhyam_bibaran, req.body.annual_bibaran_bujhayeko_nabujhayeko, req.body.lekha_parikshyan_garaiyeko_nagariyeko,req.body.created_by, updated_by, req.params.ActivitiesInfoId],
@@ -68,10 +68,10 @@ module.exports = {
 //Service for deleting a ActivitiesInfo
 module.exports = {
   deleteActivitiesInfo: (callBack) => {
-    const deleteActivitiesInfoQuery = `DELETE  FROM activities_info where yearly_activities_info_id=$1`;
+    const deleteActivitiesInfoQuery = `DELETE  FROM activities_info where activities_info_id=$1`;
     pool.query(
       deleteActivitiesInfoQuery,
-      [req.params.ActivitiesInfoId],
+      [req.params.activitiesInfoId],
       (error, results, fields) => {
         if (error) {
           callBack(error);
