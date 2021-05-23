d0170e6b98aaf3c9751158f0b8236aa548eae12c
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-05-23 14:57:36 +0545

    [FM-60]services  update  banyajantuXetiBibaran

--- server/app/services/banyajantuXetiRahatServices.js
+++ server/app/services/banyajantuXetiRahatServices.js
@@ -1,10 +1,10 @@
 const pool = require("../db");
 
-//Service for Listing all BanyajantuXetiRahat
+//Service for Listing all BanyajantuXetiBibaran
 module.exports = {
-  getAllBanyajantuXetiRahat: (callBack) => {
-    const getAllBanyajantuXetiRahatQuery = `select * from banya_jantu_xeti_rahat`;
-    pool.query(getAllBanyajantuXetiRahatQuery, [], (error, results, fields) => {
+  getAllBanyajantuXetiBibaran: (callBack) => {
+    const getAllBanyajantuXetiBibaranQuery = `select * from banyajantu_xeti_bibaran`;
+    pool.query(getAllBanyajantuXetiBibaranQuery, [], (error, results, fields) => {
       if (error) {
         callBack(error);
       }
@@ -14,13 +14,13 @@ module.exports = {
   },
 };
 
-//Service for Listing a BanyajantuXetiRahat
+//Service for Listing a BanyajantuXetiBibaran
 module.exports = {
-  getBanyajantuXetiRahat: (callBack) => {
-    const getBanyajantuXetiRahat = `select * from banya_jantu_xeti_rahat where banya_jantu_xeti_rahat_id=$1`;
+  getBanyajantuXetiBibaran: (callBack) => {
+    const getBanyajantuXetiBibaran = `select * from banyajantu_xeti_bibaran where banyajantu_xeti_bibaran_id=$1`;
     pool.query(
-      getBanyajantuXetiRahatQuery,
-      [req.params.banyajantuXetiRahatId],
+      getBanyajantuXetiBibaranQuery,
+      [req.params.banyajantuXetiBibaranId],
       (error, results, fields) => {
         if (error) {
           callBack(error);
@@ -31,12 +31,12 @@ module.exports = {
   },
 };
 
-//Service for adding a BanyajantuXetiRahat
+//Service for adding a BanyajantuXetiBibaran
 module.exports = {
-  addBanyajantuXetiRahat: (callBack) => {
-    const addBanyajantuXetiRahatQuery = `INSERT INTO banya_jantu_xeti_rahat (pidit_name, pidit_address, jagga_bibaran, nagarikta_no, upabhokta_samiti_name, xeti_garne_animal, xeti_miti, pasudhan_ghargoth, man_injury_samanya, man_injury_medium, man_death, mag_rakam, samitiko_mulyankan_rakam, vuktani_rakam, remarks,created_by, updated_by) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17) returning *`;
+  addBanyajantuXetiBibaran: (callBack) => {
+    const addBanyajantuXetiBibaranQuery = `INSERT INTO banyajantu_xeti_bibaran (pidit_name, pidit_address, jagga_bibaran, nagarikta_no, upabhokta_samiti_name, xeti_garne_animal, xeti_miti, pasudhan_ghargoth, man_injury_samanya, man_injury_medium, man_death, mag_rakam, samitiko_mulyankan_rakam, vuktani_rakam, remarks,created_by, updated_by) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17) returning *`;
     pool.query(
-      addBanyajantuXetiRahatQuery,
+      addBanyajantuXetiBibaranQuery,
       [req.body.pidit_name, req.body.pidit_address, req.body.jagga_bibaran, req.body.nagarikta_no, req.body.upabhokta_samiti_name, req.body.xeti_garne_animal, req.body.xeti_miti, req.body.pasudhan_ghargoth, req.body.man_injury_samanya, req.body.man_injury_medium, req.body.man_death, req.body.mag_rakam, req.body.samitiko_mulyankan_rakam, req.body.vuktani_rakam, req.body.remarks,req.body.created_by, req.body.updated_by],
       (error, results, fields) => {
         if (error) {
@@ -48,13 +48,13 @@ module.exports = {
   },
 };
 
-//Service for updating BanyajantuXetiRahat
+//Service for updating BanyajantuXetiBibaran
 module.exports = {
-  updateBanyajantuXetiRahat: (callBack) => {
-    const updateBanyajantuXetiRahatQuery = `UPDATE banya_jantu_xeti_rahat SET pidit_name=$1, pidit_address=$2, jagga_bibaran=$3, nagarikta_no=$4, upabhokta_samiti_name=$5, xeti_garne_animal=$6, xeti_miti=$7, pasudhan_ghargoth=$8, man_injury_samanya=$9, man_injury_medium=$10, man_death=$11, mag_rakam=$12, samitiko_mulyankan_rakam=$13, vuktani_rakam=$14, remarks=$15,created_by=$16, updated_by=$17 returning *`;
+  updateBanyajantuXetiBibaran: (callBack) => {
+    const updateBanyajantuXetiBibaranQuery = `UPDATE banyajantu_xeti_bibaran SET pidit_name=$1, pidit_address=$2, jagga_bibaran=$3, nagarikta_no=$4, upabhokta_samiti_name=$5, xeti_garne_animal=$6, xeti_miti=$7, pasudhan_ghargoth=$8, man_injury_samanya=$9, man_injury_medium=$10, man_death=$11, mag_rakam=$12, samitiko_mulyankan_rakam=$13, vuktani_rakam=$14, remarks=$15,created_by=$16, updated_by=$17 returning *`;
     pool.query(
-      updateBanyajantuXetiRahatQuery,
-      [req.body.req.body.pidit_name, req.body.pidit_address, req.body.jagga_bibaran, req.body.nagarikta_no, req.body.upabhokta_samiti_name, req.body.xeti_garne_animal, req.body.xeti_miti, req.body.pasudhan_ghargoth, req.body.man_injury_samanya, req.body.man_injury_medium, req.body.man_death, req.body.mag_rakam, req.body.samitiko_mulyankan_rakam, req.body.vuktani_rakam, req.body.remarks,req.body.created_by, req.body.updated_by, req.params.banyajantuXetiRahatId],
+      updateBanyajantuXetiBibaranQuery,
+      [req.body.req.body.pidit_name, req.body.pidit_address, req.body.jagga_bibaran, req.body.nagarikta_no, req.body.upabhokta_samiti_name, req.body.xeti_garne_animal, req.body.xeti_miti, req.body.pasudhan_ghargoth, req.body.man_injury_samanya, req.body.man_injury_medium, req.body.man_death, req.body.mag_rakam, req.body.samitiko_mulyankan_rakam, req.body.vuktani_rakam, req.body.remarks,req.body.created_by, req.body.updated_by, req.params.banyajantuXetiBibaranId],
       (error, results, fields) => {
         if (error) {
           callBack(error);
@@ -65,13 +65,13 @@ module.exports = {
   },
 };
 
-//Service for deleting BanyajantuXetiRahat
+//Service for deleting BanyajantuXetiBibaran
 module.exports = {
-  deleteBanyajantuXetiRahat: (callBack) => {
-    const deleteBanyajantuXetiRahatQuery = `DELETE  FROM banya_jantu_xeti_rahat where BanyajantuXetiRahat_id=$1`;
+  deleteBanyajantuXetiBibaran: (callBack) => {
+    const deleteBanyajantuXetiBibaranQuery = `DELETE  FROM banyajantu_xeti_bibaran where banyajantu_xeti_bibaran_id=$1`;
     pool.query(
-      deleteBanyajantuXetiRahatQuery,
-      [req.params.banyajantuXetiRahatId],
+      deleteBanyajantuXetiBibaranQuery,
+      [req.params.banyajantuXetiBibaranId],
       (error, results, fields) => {
         if (error) {
           callBack(error);
