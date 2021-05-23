0a595ee6999fd9520480c67b30fc947d257331dd
Shreedhar Acharya <spacharya2537@gmail.com>
2021-05-23 12:14:57 +0545

    [FM-83]Migration for BanXetra Atikraman Sambhandhi Bibaran

---
+++ server/migrations/20210523062757-create-banxetra-atikraman.js
@@ -0,0 +1,48 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("banxetra_atikramans", {
+      banxetra_atikraman_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      atikramit_area: {
+        type: Sequelize.STRING,
+      },
+      atikraman_kisim: {
+        type: Sequelize.STRING,
+      },
+      samalagna_ghardhuri: {
+        type: Sequelize.STRING,
+      },
+      atikraman_prayojan: {
+        type: Sequelize.STRING,
+      },
+      samrachana_bibaran: {
+        type: Sequelize.STRING,
+      },
+      atikraman_abastha: {
+        type: Sequelize.STRING,
+      },
+      created_by: {
+        type: Sequelize.STRING,
+      },
+      updated_by: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("banxetra_atikramans");
+  },
+};
