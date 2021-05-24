3ae833d781f9da221e649cd5e97a7f3a45caed64
Shreedhar Acharya <spacharya2537@gmail.com>
2021-05-24 19:59:18 +0545

    [FM-86]fixing conflicts

--- server/app/controller/provinceController.js
+++ server/app/controller/provinceController.js
@@ -1,72 +1,80 @@
-const db = require("../db");
-const {
+const pool = require("../db");
+
+// controller for getting all Province
+async function getAllProvince(req, res) {
+  const getAllProvinceQuery = `select * from provinces`;
+  pool.query(getAllProvinceQuery, [], (error, results, fields) => {
+    if (error) throw error;
+    res.send(JSON.stringify({ status: 200, error: null, data: results }));
+  });
+}
+
+// controller for getting a Province
+async function getProvince(req, res) {
+  const getProvinceQuery = "select * from provinces where prov_id=?";
+  await pool.query(
+    getProvinceQuery,
+    [req.params.provinceId],
+    (error, results, fields) => {
+      if (error) throw error;
+      res.send(JSON.stringify({ status: 200, error: null, data: results }));
+    }
+  );
+}
+
+//Service for adding a Province
+
+async function addProvince(req, res) {
+  const addProvinceQuery = `INSERT INTO provinces (prov_name_eng,prov_name_nep) values (?,?)`;
+  await pool.query(
+    addProvinceQuery,
+    [req.body.prov_name_eng, req.body.prov_name_nep],
+    (error, results, fields) => {
+      if (error) {
+        throw error;
+      }
+      res.send(JSON.stringify({ status: 200, error: null, data: results }));
+    }
+  );
+}
+
+//Controller for updating a Province
+
+async function updateProvince(req, res) {
+  console.log("dfadfadkj");
+  const updateProvinceQuery = `UPDATE provinces SET prov_name_eng=?, prov_name_nep=? WHERE prov_id=?`;
+  await pool.query(
+    updateProvinceQuery,
+    [req.body.prov_name_eng, req.body.prov_name_nep, req.params.provinceId],
+    (error, results, fields) => {
+      if (error) {
+        throw error;
+      }
+      res.send(JSON.stringify({ status: 200, error: null, data: results }));
+    }
+  );
+}
+
+//Controller for deleting a Province
+
+async function deleteProvince(req, res) {
+  const deleteProvinceQuery = `DELETE  FROM provinces where prov_id=?`;
+  pool.query(
+    deleteProvinceQuery,
+    [req.params.provinceId],
+    (error, results, fields) => {
+      if (error) {
+        throw error;
+      }
+      res.send(JSON.stringify({ status: 200, error: null, data: results }));
+    }
+  );
+}
+
+module.exports = {
   getAllProvince,
   getProvince,
   addProvince,
   updateProvince,
   deleteProvince,
-} = require("../services/provinceServices");
-
-module.exports = {
-  getAllProvince: (req, res) => {
-    getAllProvince((err, results) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("results", results);
-      return res.json(results);
-    });
-  },
-};
-
-module.exports = {
-  getProvince: (req, res) => {
-    getProvince((err, results) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("results", results);
-      return res.json(results);
-    });
-  },
-};
-
-module.exports = {
-  addProvince: (req, res) => {
-    addProvince((err, results) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("results", results);
-      return res.json(results);
-    });
-  },
-};
-
-module.exports = {
-  updateProvince: (req, res) => {
-    updateProvince((err, results) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("results", results);
-      return res.json(results);
-    });
-  },
-};
-module.exports = {
-  deleteProvince: (req, res) => {
-    deleteProvince((err, results) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("results", results);
-      return res.json(results);
-    });
-  },
 };
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 /**
  * Created by Shreedhar Acharya
  *
- * 
+ *
  */
 
 require("dotenv").config();
@@ -18,61 +18,65 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-const staffRoutes = require("./app/routes/staffRoutes");
 const provinceRoutes = require("./app/routes/provinceRoutes");
-const districtRoutes = require("./app/routes/districtRoutes");
-const municipalityRoutes = require("./app/routes/municipalityRoutes");
-const postRoutes = require("./app/routes/postRoutes");
-const levelRoutes = require("./app/routes/levelRoutes");
-const nijibanBibaranRoutes = require("./app/routes/nijibanBibaranRoutes");
-const dharmikbanBibaranRoutes = require("./app/routes/dharmikbanBibaranRoutes");
-const samudayikbanBibaranRoutes = require("./app/routes/samudayikbanBibaranRoutes");
-const banpaidawarLilamRoutes = require("./app/routes/banpaidawarLilamRoutes");
-const kabuliyatibanBibaranRoutes = require("./app/routes/kabuliyatibanBibaranRoutes");
-const upavoktasamuhaBibaranRoutes = require("./app/routes/upavoktasamuhaBibaranRoutes");
-const banxetraAtikramanAreaBibaranRoutes = require("./app/routes/banxetraAtikramanAreaBibaranRoutes");
-const bandadeloBibaranRoutes = require("./app/routes/bandadeloBibaranRoutes");
-const biruwaUtpadanRoutes = require("./app/routes/biruwanUtpadanRoutes");
-const banpaidawarRoutes = require("./app/routes/banpaidawarRoutes");
-const activitiesInfoRoutes = require("./app/routes/activitiesInfoRoutes");
-const vehiclesRoutes = require("./app/routes/vehiclesRoutes");
-const banyajantuXetiBibaranRoutes = require("./app/routes/banyajantuXetiBibaranRoutes");
-const banxetraAnyaPrayojanRoutes = require("./app/routes/banxetraAnyaPrayojanRoutes");
-const banyajantuUddarRoutes = require("./app/routes/banyajantuUddarRoutes");
-const assetsRoutes = require("./app/routes/assetsRoutes");
-const baramaditChijbastuRoutes = require("./app/routes/baramaditChijbastuRoutes");
-const departmentRoutes = require("./app/routes/departmentRoutes");
-const nabikaranKaryayojanaRoutes = require("./app/routes/nabikaranKaryayojanaRoutes");
-const muddaAnusandhanDayariRoutes = require("./app/routes/muddaAnusandhanDayariRoutes");
-const employeeHistoryRoutes = require("./app/routes/employeeHistoryRoutes");
 
-app.use("/api/v1", staffRoutes);
+//Commenting this block of code until unit test is done
+
+// const districtRoutes = require("./app/routes/districtRoutes");
+// const municipalityRoutes = require("./app/routes/municipalityRoutes");
+// const postRoutes = require("./app/routes/postRoutes");
+// const levelRoutes = require("./app/routes/levelRoutes");
+// const nijibanBibaranRoutes = require("./app/routes/nijibanBibaranRoutes");
+// const dharmikbanBibaranRoutes = require("./app/routes/dharmikbanBibaranRoutes");
+// const samudayikbanBibaranRoutes = require("./app/routes/samudayikbanBibaranRoutes");
+// const banpaidawarLilamRoutes = require("./app/routes/banpaidawarLilamRoutes");
+// const kabuliyatibanBibaranRoutes = require("./app/routes/kabuliyatibanBibaranRoutes");
+// const upavoktasamuhaBibaranRoutes = require("./app/routes/upavoktasamuhaBibaranRoutes");
+// const banxetraAtikramanAreaBibaranRoutes = require("./app/routes/banxetraAtikramanAreaBibaranRoutes");
+// const bandadeloBibaranRoutes = require("./app/routes/bandadeloBibaranRoutes");
+// const biruwaUtpadanRoutes = require("./app/routes/biruwanUtpadanRoutes");
+// const banpaidawarRoutes = require("./app/routes/banpaidawarRoutes");
+// const activitiesInfoRoutes = require("./app/routes/activitiesInfoRoutes");
+// const vehiclesRoutes = require("./app/routes/vehiclesRoutes");
+// const banyajantuXetiBibaranRoutes = require("./app/routes/banyajantuXetiBibaranRoutes");
+// const banxetraAnyaPrayojanRoutes = require("./app/routes/banxetraAnyaPrayojanRoutes");
+// const banyajantuUddarRoutes = require("./app/routes/banyajantuUddarRoutes");
+// const assetsRoutes = require("./app/routes/assetsRoutes");
+// const baramaditChijbastuRoutes = require("./app/routes/baramaditChijbastuRoutes");
+// const departmentRoutes = require("./app/routes/departmentRoutes");
+// const nabikaranKaryayojanaRoutes = require("./app/routes/nabikaranKaryayojanaRoutes");
+// const muddaAnusandhanDayariRoutes = require("./app/routes/muddaAnusandhanDayariRoutes");
+// const employeeHistoryRoutes = require("./app/routes/employeeHistoryRoutes");
+
 app.use("/api/v1", provinceRoutes);
-app.use("/app/v1", districtRoutes);
-app.use("/app/v1", municipalityRoutes);
-app.use("/api/v1", postRoutes);
-app.use("/api/v1", levelRoutes);
-app.use("/api/v1",nijibanBibaranRoutes);
-app.use("/api/v1",dharmikbanBibaranRoutes);
-app.use("/api/v1",samudayikbanBibaranRoutes);
-app.use("/api/v1",banpaidawarLilamRoutes);
-app.use("/api/v1",kabuliyatibanBibaranRoutes);
-app.use("/api/v1",upavoktasamuhaBibaranRoutes);
-app.use("/api/v1",banxetraAtikramanAreaBibaranRoutes);
-app.use("/api/v1",bandadeloBibaranRoutes);
-app.use("/api/v1",biruwaUtpadanRoutes);
-app.use("/api/v1",banpaidawarRoutes);
-app.use("/api/v1",activitiesInfoRoutes);
-app.use("/api/v1",vehiclesRoutes);
-app.use("/api/v1",banyajantuXetiBibaranRoutes);
-app.use("/api/v1",banxetraAnyaPrayojanRoutes);
-app.use("/api/v1",banyajantuUddarRoutes);
-app.use("/api/v1", assetsRoutes);
-app.use("/api/v1",baramaditChijbastuRoutes);
-app.use("/api/v1",departmentRoutes);
-app.use("/api/v1",nabikaranKaryayojanaRoutes);
-app.use("/api/v1",muddaAnusandhanDayariRoutes);
-app.use("/api/v1", employeeHistoryRoutes);
+
+//Commenting this block of code until unit test is done
+
+// app.use("/app/v1", districtRoutes);
+// app.use("/app/v1", municipalityRoutes);
+// app.use("/api/v1", postRoutes);
+// app.use("/api/v1", levelRoutes);
+// app.use("/api/v1",nijibanBibaranRoutes);
+// app.use("/api/v1",dharmikbanBibaranRoutes);
+// app.use("/api/v1",samudayikbanBibaranRoutes);
+// app.use("/api/v1",banpaidawarLilamRoutes);
+// app.use("/api/v1",kabuliyatibanBibaranRoutes);
+// app.use("/api/v1",upavoktasamuhaBibaranRoutes);
+// app.use("/api/v1",banxetraAtikramanAreaBibaranRoutes);
+// app.use("/api/v1",bandadeloBibaranRoutes);
+// app.use("/api/v1",biruwaUtpadanRoutes);
+// app.use("/api/v1",banpaidawarRoutes);
+// app.use("/api/v1",activitiesInfoRoutes);
+// app.use("/api/v1",vehiclesRoutes);
+// app.use("/api/v1",banyajantuXetiBibaranRoutes);
+// app.use("/api/v1",banxetraAnyaPrayojanRoutes);
+// app.use("/api/v1",banyajantuUddarRoutes);
+// app.use("/api/v1", assetsRoutes);
+// app.use("/api/v1",baramaditChijbastuRoutes);
+// app.use("/api/v1",departmentRoutes);
+// app.use("/api/v1",nabikaranKaryayojanaRoutes);
+// app.use("/api/v1",muddaAnusandhanDayariRoutes);
+// app.use("/api/v1", employeeHistoryRoutes);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
