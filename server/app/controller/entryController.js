618a34d9da9c0697d095c59d4c02c2cc9cdf1893
Subash Paudel <subashpaudel144@gmail.com>
2021-05-24 14:02:16 +0545

    [FM-34] new file entryController.js added

---
+++ server/app/controller/entryController.js
@@ -0,0 +1,72 @@
+const db = require("../db");
+const {
+  getAllEntry,
+  getEntry,
+  addEntry,
+  updateEntry,
+  deleteEntry,
+} = require("../services/entryServices");
+
+module.exports = {
+  getAllEntry: (req, res) => {
+    getAllEntry((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("results", results);
+      return res.json(results);
+    });
+  },
+};
+
+module.exports = {
+  getEntry: (req, res) => {
+    getEntry((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("results", results);
+      return res.json(results);
+    });
+  },
+};
+
+module.exports = {
+  addEntry: (req, res) => {
+    addEntry((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("results", results);
+      return res.json(results);
+    });
+  },
+};
+
+module.exports = {
+  updateEntry: (req, res) => {
+    updateEntry((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("results", results);
+      return res.json(results);
+    });
+  },
+};
+module.exports = {
+  deleteEntry: (req, res) => {
+    deleteEntry((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("results", results);
+      return res.json(results);
+    });
+  },
+};
