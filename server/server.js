ce6aac8c7fa5c94c3ed7b0004ec9fd083a7cb502
Subash Paudel <subashpaudel144@gmail.com>
2021-05-26 15:55:26 +0545

    [FM-28] routes of municipalities uncommented

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const provinceRoutes = require("./app/routes/provinceRoutes");
 //Commenting this block of code until unit test is done
 
 const districtRoutes = require("./app/routes/districtRoutes");
-// const municipalityRoutes = require("./app/routes/municipalityRoutes");
+const municipalitiesRoutes = require("./app/routes/municipalitiesRoutes");
 // const postRoutes = require("./app/routes/postRoutes");
 const levelRoutes = require("./app/routes/levelRoutes");
 // const nijibanBibaranRoutes = require("./app/routes/nijibanBibaranRoutes");
@@ -55,7 +55,7 @@ app.use("/api/v1", provinceRoutes);
 //Commenting this block of code until unit test is done
 
 app.use("/api/v1", districtRoutes);
-// app.use("/app/v1", municipalityRoutes);
+app.use("/api/v1", municipalitiesRoutes);
 // app.use("/api/v1", postRoutes);
 app.use("/api/v1", levelRoutes);
 // app.use("/api/v1",nijibanBibaranRoutes);
