e1ca7d49bea7647aa1edbf149646ae4bc4508c4f
Subash Paudel <subashpaudel144@gmail.com>
2021-05-27 22:54:17 +0545

    [FM-72] mudda anusandhan dayari controller file modified

--- server/app/controller/muddaAnusandhanDayariController.js
+++ server/app/controller/muddaAnusandhanDayariController.js
@@ -1,73 +1,115 @@
-const db = require("../db");
-const {
-  getAllMuddaAnusandhanDayari,
-  getMuddaAnusandhanDayari,
-  addMuddaAnusandhanDayari,
-  updateMuddaAnusandhanDayari,
-  deleteMuddaAnusandhanDayari,
-} = require("../services/muddaAnusandhanDayariServices");
+const pool = require("../db");
+
+//Controller for Listing all Mudda Anusandhan Dayaris
+async function getAllMuddaAnusandhanDayaris(req, res) {
+  const getAllMuddaAnusandhanDayarisQuery = `select * from mudda_anusandhan_dayaris`;
+  pool.query(getAllMuddaAnusandhanDayarisQuery, [], (error, results, fields) => {
+    if (error) throw error;
+    res.send(JSON.stringify({ status: 200, error: null, data: results }));
+  });
+}
+
+//Controller for Listing a Mudda Anusandhan Dayari
+async function getMuddaAnusandhanDayaris(req, res) {
+  const getMuddaAnusandhanDayarisQuery = `select * from mudda_anusandhan_dayaris where mudda_anusandhan_dayari_id=?`;
+  pool.query(getMuddaAnusandhanDayarisQuery, [req.params.muddaAnusandhanDayariId], (error, results, fields) => {
+    if (error) throw error;
+    res.send(JSON.stringify({ status: 200, error: null, data: results }));
+  });
+}
+
+//Controller for adding a Mudda Anusandhan Dayari
+async function addMuddaAnusandhanDayaris(req, res) {
+  const addMuddaAnusandhanDayarisQuery = `INSERT INTO mudda_anusandhan_dayaris (jaheri_partibedan_miti,kasurko_kisim,bigo_pariman,jaggako_area,jaggako_thegana,abhiyog_miti,abhiyog_nikaya,abhiyog_jariwana,kaid,bojbahak_jafat_maagdabi,pratibadi_sankhya,thunchek_dharauti,sadharan_tarekh,thuna_aadhes,faisala_miti,faisala_jariwana,faisala_kaid,bojbahak_jafat,created_by,updated_by) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+  pool.query(
+    addMuddaAnusandhanDayarisQuery,
+    [
+      req.body.jaheri_partibedan_miti,
+      req.body.kasurko_kisim,
+      req.body.bigo_pariman,
+      req.body.jaggako_area,
+      req.body.jaggako_thegana,
+      req.body.abhiyog_miti,
+      req.body.abhiyog_nikaya,
+      req.body.abhiyog_jariwana,
+      req.body.kaid,
+      req.body.bojbahak_jafat_maagdabi,
+      req.body.pratibadi_sankhya,
+      req.body.thunchek_dharauti,
+      req.body.sadharan_tarekh,
+      req.body.thuna_aadhes,
+      req.body.faisala_miti,
+      req.body.faisala_jariwana,
+      req.body.faisala_kaid,
+      req.body.bojbahak_jafat,
+      req.body.created_by,
+      req.body.updated_by,
+    ],
+    (error, results, fields) => {
+      if (error) {
+        throw error;
+      }
+      res.send(JSON.stringify({ status: 200, error: null, data: results }));
+    }
+  );
+}
+
+//Controller for updating a Mudda Anusandhan Dayari
+async function updateMuddaAnusandhanDayaris(req, res) {
+  const updateMuddaAnusandhanDayarisQuery = `UPDATE mudda_anusandhan_dayaris SET jaheri_partibedan_miti=?,kasurko_kisim=?,bigo_pariman=?,jaggako_area=?,jaggako_thegana=?,abhiyog_miti=?,abhiyog_nikaya=?,abhiyog_jariwana=?,kaid=?,bojbahak_jafat_maagdabi=?,pratibadi_sankhya=?,thunchek_dharauti=?,sadharan_tarekh=?,thuna_aadhes=?,faisala_miti=?,faisala_jariwana=?,faisala_kaid=?,bojbahak_jafat=?,created_by=?,updated_by=? WHERE mudda_anusandhan_dayari_id=?`;
+  pool.query(
+    updateMuddaAnusandhanDayarisQuery,
+    [
+      req.body.jaheri_partibedan_miti,
+      req.body.kasurko_kisim,
+      req.body.bigo_pariman,
+      req.body.jaggako_area,
+      req.body.jaggako_thegana,
+      req.body.abhiyog_miti,
+      req.body.abhiyog_nikaya,
+      req.body.abhiyog_jariwana,
+      req.body.kaid,
+      req.body.bojbahak_jafat_maagdabi,
+      req.body.pratibadi_sankhya,
+      req.body.thunchek_dharauti,
+      req.body.sadharan_tarekh,
+      req.body.thuna_aadhes,
+      req.body.faisala_miti,
+      req.body.faisala_jariwana,
+      req.body.faisala_kaid,
+      req.body.bojbahak_jafat,
+      req.body.created_by,
+      req.body.updated_by,
+      req.params.muddaAnusandhanDayariId,
+    ],
+    (error, results, fields) => {
+      if (error) {
+        throw error;
+      }
+      res.send(JSON.stringify({ status: 200, error: null, data: results }));
+    }
+  );
+}
+
+//Controller for deleting a Department
+async function deleteMuddaAnusandhanDayaris(req, res) {
+  const deleteMuddaAnusandhanDayarisQuery = `DELETE  FROM mudda_anusandhan_dayaris WHERE mudda_anusandhan_dayari_id=?`;
+  pool.query(
+    deleteMuddaAnusandhanDayarisQuery,
+    [req.params.muddaAnusandhanDayariId],
+    (error, results, fields) => {
+      if (error) {
+        throw error;
+      }
+      res.send(JSON.stringify({ status: 200, error: null, data: results }));
+    }
+  );
+}
 
 module.exports = {
-  getAllMuddaAnusandhanDayari: (req, res) => {
-    getAllMuddaAnusandhanDayari((err, results) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("results", results);
-      return res.json(results);
-    });
-  },
+  getAllMuddaAnusandhanDayaris,
+  getMuddaAnusandhanDayaris,
+  addMuddaAnusandhanDayaris,
+  updateMuddaAnusandhanDayaris,
+  deleteMuddaAnusandhanDayaris,
 };
-
-module.exports = {
-  getMuddaAnusandhanDayari: (req, res) => {
-    getMuddaAnusandhanDayari((err, results) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("results", results);
-      return res.json(results);
-    });
-  },
-};
-
-module.exports = {
-  addMuddaAnusandhanDayari: (req, res) => {
-    addMuddaAnusandhanDayari((err, results) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("results", results);
-      return res.json(results);
-    });
-  },
-};
-
-module.exports = {
-  updateMuddaAnusandhanDayari: (req, res) => {
-    updateMuddaAnusandhanDayari((err, results) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("results", results);
-      return res.json(results);
-    });
-  },
-};
-module.exports = {
-  deleteMuddaAnusandhanDayari: (req, res) => {
-    deleteMuddaAnusandhanDayari((err, results) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("results", results);
-      return res.json(results);
-    });
-  },
-};
-
