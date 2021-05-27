79d46231682660eb2edd03390548380a96e39eb9
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-05-27 13:21:09 +0545

    [FM-52] update routes  banpaidawarLilam

--- server/app/routes/banpaidawarLilamRoutes.js
+++ server/app/routes/banpaidawarLilamRoutes.js
@@ -1,10 +1,11 @@
+
 const express = require("express");
 const router = express.Router();
 
 const banpaidawarLilamController = require("../controller/banpaidawarLilamController");
-router.get("/banpaidawarLilam", provinceController.getAllBanpaidawarLilam);
-router.get("/banpaidawarLilam/banpaidawarLilamId", banpaidawarLilamController.getBanpaidawarLilam);
+router.get("/banpaidawarLilam", banpaidawarLilamController.getAllBanpaidawarLilam);
+router.get("/banpaidawarLilam/:banpaidawarLilamId", banpaidawarLilamController.getBanpaidawarLilam);
 router.post("/banpaidawarLilam", banpaidawarLilamController.addBanpaidawarLilam);
-router.put("/banpaidawarLilam", banpaidawarLilamController.updateBanpaidawarLilam);
-router.delete("/banpaidawarLilam", banpaidawarLilamController.deletebanpaidawarLilam);
+router.put("/banpaidawarLilam/:banpaidawarLilamId", banpaidawarLilamController.updateBanpaidawarLilam);
+router.delete("/banpaidawarLilam/:banpaidawarLilamId", banpaidawarLilamController.deleteBanpaidawarLilam);
 module.exports = router;
