0ece9015f18cebf6335c3a3be0668a3ed1d45a07
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-05-27 15:20:02 +0545

    [FM-79]routes update  banpaidawar

--- server/app/routes/banpaidawarRoutes.js
+++ server/app/routes/banpaidawarRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const banpaidawarController = require("../controller/banpaidawarController");
+const banpaidawarController = require("../controller/BanpaidawarController");
 router.get("/banpaidawar", banpaidawarController.getAllBanpaidawar);
-router.get("/banpaidawar/banpaidawarId", banpaidawarController.getBanpaidawar);
+router.get("/banpaidawar/:banpaidawarId", banpaidawarController.getBanpaidawar);
 router.post("/banpaidawar", banpaidawarController.addBanpaidawar);
-router.put("/banpaidawar", banpaidawarController.updateBanpaidawar);
-router.delete("/banpaidawar", banpaidawarController.deleteBanpaidawar);
+router.put("/banpaidawar/:banpaidawarId", banpaidawarController.updateBanpaidawar);
+router.delete("/banpaidawar/:banpaidawarId", banpaidawarController.deleteBanpaidawar);
 module.exports = router;
-
