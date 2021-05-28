a281d5f36c1bdc8e56ab0e41c19a76566cd68fea
Subash Paudel <subashpaudel144@gmail.com>
2021-05-28 15:12:50 +0545

    [FM-77] column name changed

--- server/migrations/20210523091304-create-biruwa-utpadan.js
+++ server/migrations/20210523091304-create-biruwa-utpadan.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("biruwa-utpadans", {
+    await queryInterface.createTable("biruwa_utpadans", {
       biruwa_utpadan_id: {
         allowNull: false,
         autoIncrement: true,
