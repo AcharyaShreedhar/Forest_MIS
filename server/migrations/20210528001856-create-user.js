e324a37f10d626c4e3c1390e8e5d2a098e46f00a
Shreedhar Acharya <spacharya2537@gmail.com>
2021-05-28 06:09:07 +0545

    [FM-14]Migration for User

---
+++ server/migrations/20210528001856-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      user_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_type: {
+        type: Sequelize.STRING
+      },
+      user_name: {
+        type: Sequelize.STRING
+      },
+      user_pass: {
+        type: Sequelize.STRING
+      },
+      created_by: {
+        type: Sequelize.STRING
+      },
+      updated_by: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
