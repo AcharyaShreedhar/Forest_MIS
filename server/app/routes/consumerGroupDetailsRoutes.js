f03ace712a5ef9cd245abef25e1c84cac75be6f6
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-05-30 14:25:45 +0545

    [FM-58]routes update consumerGroupDetails

--- server/app/routes/consumerGroupDetailsRoutes.js
+++ server/app/routes/consumerGroupDetailsRoutes.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-const ConsumerGroupDetailsController = require("../controller/ConsumerGroupDetailsController");
-router.get("/ConsumerGroupDetails", ConsumerGroupDetailsController.getAllConsumerGroupDetails);
-router.get("/ConsumerGroupDetails/:ConsumerGroupDetailsId", ConsumerGroupDetailsController.getConsumerGroupDetails);
-router.post("/ConsumerGroupDetails",ConsumerGroupDetailsController.addConsumerGroupDetails);
-router.put("/ConsumerGroupDetails/:ConsumerGroupDetailsId", ConsumerGroupDetailsController.updateConsumerGroupDetails);
-router.delete("/ConsumerGroupDetails/:ConsumerGroupDetailsId", ConsumerGroupDetailsController.deleteConsumerGroupDetails);
+const consumerGroupDetailsController = require("../controller/ConsumerGroupDetailsController");
+router.get(
+  "/ConsumerGroupDetails",
+  consumerGroupDetailsController.getAllConsumerGroupDetails
+);
+router.get(
+  "/ConsumerGroupDetails/:consumerGroupDetailsId",
+  consumerGroupDetailsController.getConsumerGroupDetails
+);
+router.post(
+  "/ConsumerGroupDetails",
+  consumerGroupDetailsController.addConsumerGroupDetails
+);
+router.put(
+  "/ConsumerGroupDetails/:consumerGroupDetailsId",
+  consumerGroupDetailsController.updateConsumerGroupDetails
+);
+router.delete(
+  "/ConsumerGroupDetails/:consumerGroupDetailsId",
+  consumerGroupDetailsController.deleteConsumerGroupDetails
+);
 module.exports = router;
