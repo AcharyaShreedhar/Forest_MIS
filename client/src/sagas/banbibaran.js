c41e73bb74f69ce900878d4105b7174dfbe71d5b
Shreedhar Acharya <spacharya2537@gmail.com>
2021-05-31 16:17:28 +0545

    [FM-94]update fetchall

--- client/src/sagas/banbibaran.js
+++ client/src/sagas/banbibaran.js
@@ -3,14 +3,13 @@ import { call, put } from "redux-saga/effects";
 import { history } from "../reducers";
 import BanbibaranActions from "../actions/banbibaran";
 
-export function* fetchsamudayikbanbibaranRequest(api, action) {
-    console.log('.......................sagas')
-  
+export function* fetchallsamudayikbanbibaranRequest(api, action) {
   const response = yield api.getSamudayikbanBibaranList();
-console.log('saga',response)
   if (response.ok) {
-    yield put(BanbibaranActions.fetchsamudayikbanbibaranSuccess(response.data));
+    yield put(
+      BanbibaranActions.fetchallsamudayikbanbibaranSuccess(response.data)
+    );
   } else {
-    yield put(BanbibaranActions.fetchsamudayikbanbibaranFailure());
+    yield put(BanbibaranActions.fetchallsamudayikbanbibaranFailure());
   }
 }
--- client/src/sagas/index.js
+++ client/src/sagas/index.js
@@ -19,8 +19,8 @@ export default function* root() {
 
     //----------------------------banbibaran
     takeLatest(
-      BanbibaranTypes.FETCHSAMUDAYIKBANBIBARAN_REQUEST,
-      fetchsamudayikbanbibaranRequest,
+      BanbibaranTypes.FETCHALLSAMUDAYIKBANBIBARAN_REQUEST,
+      fetchallsamudayikbanbibaranRequest,
       api
     ),
   ]);
