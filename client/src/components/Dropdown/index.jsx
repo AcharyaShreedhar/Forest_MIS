aae92ae3c18b9c0c1a4c5df169e51a7a55b62380
Shreedhar Acharya <spacharya2537@gmail.com>
2021-06-01 13:23:10 +0545

    [FM-100]Dropdown Component

---
+++ client/src/components/Dropdown/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class index extends Component {
+    render() {
+        return (
+            <div>
+                Dropdown
+            </div>
+        )
+    }
+}
+
+export default index
