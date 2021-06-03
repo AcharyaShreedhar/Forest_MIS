bf21782997a103804cbb27deff3bc9819b487208
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-06-03 13:03:40 +0545

    [FM-94]getnijibanroutes update nijibanBibaran

--- server/app/routes/nijiBanBibaranroutes.js
+++ server/app/routes/nijiBanBibaranroutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const nijibanBibaranController = require("../controller/nijibanBibaranController");
 router.get("/nijibanBibaran", nijibanBibaranController.getAllNijibanBibaran);
-router.get("/nijibanBibaran/:nijibanBibaranId", nijibanBibaranController.getAllNijibanBibaran);
+router.get("/nijibanBibaran/:nijibanBibaranId", nijibanBibaranController.getNijibanBibaran);
 router.post("/nijibanBibaran", nijibanBibaranController.addNijibanBibaran);
 router.put("/nijibanBibaran/:nijibanBibaranId", nijibanBibaranController.updateNijibanBibaran);
 router.delete("/nijibanBibaran/:nijibanBibaranId", nijibanBibaranController.deleteNijibanBibaran);
