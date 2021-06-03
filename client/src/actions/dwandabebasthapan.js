edd82a5ee12c9c9812317495a5e2d4d93d1acd02
Subash Paudel <subashpaudel144@gmail.com>
2021-06-03 16:12:39 +0545

    [FM-95] new file for dwandabebasthapan is added in action

---
+++ client/src/actions/dwandabebasthapan.js
@@ -0,0 +1,27 @@
+/**
+ * We will use the following prefixes.
+ * add: When adding something to the database.
+ * delete: When deleting something from the database.
+ * update: When updating something in the database.
+ * fetch: When pulling something from the database.
+ * search: When searching something in the database.
+ * select: When setting reducers, not handling with database, just handling only reducers.
+ */
+
+ import { createActions } from "reduxsauce";
+
+ const { Types, Creators } = createActions({
+     
+   fetchallbanyajantuuddarRequest: ["payload"],
+   fetchallbanyajantuuddarSuccess: ["response"],
+   fetchallbanyajantuuddarFailure: null,
+
+ 
+   locationsRequest: ["payload"],
+   // Clear all caches
+   clearRequest: null,
+ });
+ 
+ export const DwandabebasthapanTypes = Types;
+ export default Creators;
+ 
