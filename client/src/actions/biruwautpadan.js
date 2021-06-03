d7001d3846a95b3feed5208616411dd83f628c9f
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-06-03 16:38:26 +0545

    [FM-96]actions  update biruwautpadan

---
+++ client/src/actions/biruwautpadan.js
@@ -0,0 +1,27 @@
+/**
+ * We will use the following prefixes.
+ * add: When adding something to the database.
+ * delete: When deleting something from the database.
+ * update: When updating something in the database.
+ * fetch: When pulling something from the database.
+ * search: When searching something in the database.
+ * select: When setting reducers, not handling with database, just handling only reducers.
+ */
+
+ import { createActions } from "reduxsauce";
+
+ const { Types, Creators } = createActions({
+     
+   fetchallbiruwautpadanRequest: ["payload"],
+   fetchallbiruwautpadanSuccess: ["response"],
+   fetchallbiruwautpadanFailure: null,
+ 
+ 
+   locationsRequest: ["payload"],
+   // Clear all caches
+   clearRequest: null,
+ });
+ 
+ export const BiruwautpadanTypes = Types;
+ export default Creators;
+ 
