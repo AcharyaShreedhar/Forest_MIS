ba30ecb482c1465354e54c692b3b59746d835f24
Subash Paudel <subashpaudel144@gmail.com>
2021-06-03 16:40:39 +0545

    [FM-95] console removed in saga

--- client/src/sagas/dwandabebasthapan.js
+++ client/src/sagas/dwandabebasthapan.js
@@ -6,7 +6,6 @@ import DwandabebasthapanActions from "../actions/dwandabebasthapan";
 export function* fetchallbanyajantuuddarRequest(api, action) {
  
   const response = yield api.getBanyajantuUddarList();
-  console.log("Response....saga...", response);
   if (response.ok) {
     yield put(
         DwandabebasthapanActions.fetchallbanyajantuuddarSuccess(response.data)
