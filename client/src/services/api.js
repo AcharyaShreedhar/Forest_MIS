f6718a5eb78bd7d699433c51d0ecf84402a460b2
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-06-04 15:31:01 +0545

    [FM-96]services  update biruwautpadan_get

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,117 +4,121 @@
  * prefix 2: section name such as admin
  * samples: postAdminStaffSave
  */
-import apisauce from "apisauce";
-import { equals, isNil, isEmpty } from "ramda";
-import { store } from "../reducers";
-import AppActions from "../actions/app";
-
-const Config = {
-  API_URL: "http://localhost:3001/api/v1/",
-};
-
-// const authenticated = (api) => {
-//   api.setHeader("Authorization", "Bearer " + window.token);
-
-//   return api;
-// };
-
-const naviMonitor = (response) => {
-  if (equals(response.status, 401)) {
-    store.dispatch(AppActions.logoutRequest());
-    console.log("Your token has been expired.", response.config.url);
-  }
-};
-
-const create = (baseURL = Config.API_URL) => {
-  const api = apisauce.create({
-    baseURL,
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    // 50 second timeout...
-    timeout: 50000,
-  });
-
-  const s3API = apisauce.create({
-    baseURL: Config.S3_URL,
-    headers: {
-      Accept: "application/xml",
-      "Content-Type": "multipart/form-data",
-    },
-    // 50 second timeout...
-    timeout: 50000,
-  });
-
-  api.addMonitor(naviMonitor);
-
-  // Login API
-  const loginByUsername = (payload) => api.post("auth/login", payload);
-
-  //Banbibarans
-  const getSamudayikbanBibaranList = () =>
-    api.get("samudayikbanBibaran");
-
-  const getSamudayikbanBibaran = samudayikbanBibaranId => 
-    api.get(`samudayikbanBibaran/${samudayikbanBibaranId}`);
-
-  const getDharmikbanBibaranList = () =>
-    api.get("dharmikbanBibaran");
-
-  const getDharmikbanBibaran = dharmikbanBibaranId => 
-    api.get(`dharmikbanBibaran/${dharmikbanBibaranId}`);
+ import apisauce from "apisauce";
+ import { equals, isNil, isEmpty } from "ramda";
+ import { store } from "../reducers";
+ import AppActions from "../actions/app";
+ 
+ const Config = {
+   API_URL: "http://localhost:3001/api/v1/",
+ };
+ 
+ // const authenticated = (api) => {
+ //   api.setHeader("Authorization", "Bearer " + window.token);
+ 
+ //   return api;
+ // };
+ 
+ const naviMonitor = (response) => {
+   if (equals(response.status, 401)) {
+     store.dispatch(AppActions.logoutRequest());
+     console.log("Your token has been expired.", response.config.url);
+   }
+ };
+ 
+ const create = (baseURL = Config.API_URL) => {
+   const api = apisauce.create({
+     baseURL,
+     headers: {
+       Accept: "application/json",
+       "Content-Type": "application/json",
+     },
+     // 50 second timeout...
+     timeout: 50000,
+   });
+ 
+   const s3API = apisauce.create({
+     baseURL: Config.S3_URL,
+     headers: {
+       Accept: "application/xml",
+       "Content-Type": "multipart/form-data",
+     },
+     // 50 second timeout...
+     timeout: 50000,
+   });
+ 
+   api.addMonitor(naviMonitor);
+ 
+   // Login API
+   const loginByUsername = (payload) => api.post("auth/login", payload);
+ 
+   //Banbibarans
+   const getSamudayikbanBibaranList = () =>
+     api.get("samudayikbanBibaran");
+ 
+   const getSamudayikbanBibaran = samudayikbanBibaranId => 
+     api.get(`samudayikbanBibaran/${samudayikbanBibaranId}`);
+ 
+   const getDharmikbanBibaranList = () =>
+     api.get("dharmikbanBibaran");
+ 
+   const getDharmikbanBibaran = dharmikbanBibaranId => 
+     api.get(`dharmikbanBibaran/${dharmikbanBibaranId}`);
+     
+   const getNijibanBibaranList = () =>
+     api.get("nijibanBibaran");
+ 
+   const getNijibanBibaran = nijibanBibaranId => 
+     api.get(`nijibanBibaran/${nijibanBibaranId}`);
+ 
+   const getKabuliyatibanBibaranList = () =>
+     api.get("kabuliyatibanBibaran");
+ 
+   const getKabuliyatibanBibaran = kabuliyatibanBibaranId => 
+     api.get(`kabuliyatibanBibaran/${kabuliyatibanBibaranId}`);
     
-  const getNijibanBibaranList = () =>
-    api.get("nijibanBibaran");
-
-  const getNijibanBibaran = nijibanBibaranId => 
-    api.get(`nijibanBibaran/${nijibanBibaranId}`);
-
-  const getKabuliyatibanBibaranList = () =>
-    api.get("kabuliyatibanBibaran");
-
-  const getKabuliyatibanBibaran = kabuliyatibanBibaranId => 
-    api.get(`kabuliyatibanBibaran/${kabuliyatibanBibaranId}`);
-   
-  const getBiruwautpadanList = () =>
-    api.get("biruwaUtpadans");
-
-
-    //Dwandabebasthapan
-
-  const getBanyajantuUddarList = () =>
-    api.get("banyajantuUddars");
-
-  const getBanyajantuUddar = banyajantuUddarId =>
-    api.get(`banyajantuUddars/${banyajantuUddarId}`);
-
-  const getBanyajantuXetiList = () =>
-    api.get("banyajantuXetiBibarans");
-    
-
-  return {
-    loginByUsername,
-    getSamudayikbanBibaranList,
-    getSamudayikbanBibaran,
-    getDharmikbanBibaranList,
-    getDharmikbanBibaran,
-    getNijibanBibaranList,   
-    getNijibanBibaran,    
-    getKabuliyatibanBibaranList,
-    getKabuliyatibanBibaran,
-
-    //dwandabebasthapan
-    getBanyajantuUddarList,
-    getBanyajantuUddar,
-    getBanyajantuXetiList,
-
-    //biruwautpadans
-    getBiruwautpadanList,
-  
-  };
-};
-
-export default {
-  create,
-};
+   const getBiruwautpadanList = () =>
+     api.get("biruwaUtpadans");
+ 
+   const getBiruwautpadan = biruwautpadanId => 
+     api.get(`biruwaUtpadans/${biruwautpadanId}`);
+ 
+ 
+     //Dwandabebasthapan
+ 
+   const getBanyajantuUddarList = () =>
+     api.get("banyajantuUddars");
+ 
+   const getBanyajantuUddar = banyajantuUddarId =>
+     api.get(`banyajantuUddars/${banyajantuUddarId}`);
+ 
+   const getBanyajantuXetiList = () =>
+     api.get("banyajantuXetiBibarans");
+     
+ 
+   return {
+     loginByUsername,
+     getSamudayikbanBibaranList,
+     getSamudayikbanBibaran,
+     getDharmikbanBibaranList,
+     getDharmikbanBibaran,
+     getNijibanBibaranList,   
+     getNijibanBibaran,    
+     getKabuliyatibanBibaranList,
+     getKabuliyatibanBibaran,
+ 
+     //dwandabebasthapan
+     getBanyajantuUddarList,
+     getBanyajantuUddar,
+     getBanyajantuXetiList,
+ 
+     //biruwautpadans
+     getBiruwautpadanList,
+     getBiruwautpadan,
+   };
+ };
+ 
+ export default {
+   create,
+ };
+ 
