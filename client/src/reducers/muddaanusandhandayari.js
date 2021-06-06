803747733752fb6e03d78ccb788a23ded10e48d5
Subash Paudel <subashpaudel144@gmail.com>
2021-06-06 13:28:53 +0545

    [FM-127] new file muddaanusandhandayari added in reducer

---
+++ client/src/reducers/muddaanusandhandayari.js
@@ -0,0 +1,48 @@
+import { createReducer } from "reduxsauce";
+import Immutable from "seamless-immutable";
+import { dropLast, prepend } from "ramda";
+import { MuddaanusandhandayariTypes } from "../actions/muddaanusandhandayari";
+
+
+const initialState = Immutable({
+  status: "",
+  token: "",
+});
+
+const fetchallmuddaanusandhandayariRequest = (state, action) =>
+  state.merge({ ...state, token: "", status: "pending" });
+const fetchallmuddaanusandhandayariSuccess = (state, action) => {
+
+  return state.merge({
+    ...state,
+    status: "done",
+    allmuddaanusandhandayariData: action.response,
+  });
+};
+const fetchallmuddaanusandhandayariFailure = (state, action) => {
+  state.merge({ ...state, status: "error" });
+};
+
+
+const locationsRequest = (state, action) => {
+  let locations = state.locations;
+
+  locations = prepend(action.payload.route, locations);
+  locations = dropLast(1, locations);
+  return state.merge({ ...state, locations });
+};
+
+const clearRequest = (state, action) =>
+  state.merge({ ...state, ...initialState });
+
+export const reducer = createReducer(initialState, {
+
+  [MuddaanusandhandayariTypes.FETCHALLMUDDAANUSANDHANDAYARI_REQUEST]: fetchallmuddaanusandhandayariRequest,
+  [MuddaanusandhandayariTypes.FETCHALLMUDDAANUSANDHANDAYARI_SUCCESS]: fetchallmuddaanusandhandayariSuccess,
+  [MuddaanusandhandayariTypes.FETCHALLMUDDAANUSANDHANDAYARI_FAILURE]: fetchallmuddaanusandhandayariFailure,
+
+ 
+ 
+  [MuddaanusandhandayariTypes.LOCATIONS_REQUEST]: locationsRequest,
+  [MuddaanusandhandayariTypes.CLEAR_REQUEST]: clearRequest,
+});
