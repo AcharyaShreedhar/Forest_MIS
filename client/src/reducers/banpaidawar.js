4cf58487e9044086bf9f361280c1cb084dc09928
Subash Paudel <subashpaudel144@gmail.com>
2021-06-07 13:26:17 +0545

    [FM-131] code for getall banpaidawarlilam added in reducer

--- client/src/reducers/banpaidawar.js
+++ client/src/reducers/banpaidawar.js
@@ -38,6 +38,21 @@ const fetchbanpaidawarFailure = (state, action) => {
 };
 
 
+const fetchallbanpaidawarlilamRequest = (state, action) =>
+  state.merge({ ...state, token: "", status: "pending" });
+const fetchallbanpaidawarlilamSuccess = (state, action) => {
+
+  return state.merge({
+    ...state,
+    status: "done",
+    allbanpaidawarlilamData: action.response,
+  });
+};
+const fetchallbanpaidawarlilamFailure = (state, action) => {
+  state.merge({ ...state, status: "error" });
+};
+
+
 
 const locationsRequest = (state, action) => {
   let locations = state.locations;
@@ -59,6 +74,10 @@ export const reducer = createReducer(initialState, {
   [BanpaidawarTypes.FETCHBANPAIDAWAR_REQUEST]: fetchbanpaidawarRequest,
   [BanpaidawarTypes.FETCHBANPAIDAWAR_SUCCESS]: fetchbanpaidawarSuccess,
   [BanpaidawarTypes.FETCHBANPAIDAWAR_FAILURE]: fetchbanpaidawarFailure,
+
+  [BanpaidawarTypes.FETCHALLBANPAIDAWARLILAM_REQUEST]: fetchallbanpaidawarlilamRequest,
+  [BanpaidawarTypes.FETCHALLBANPAIDAWARLILAM_SUCCESS]: fetchallbanpaidawarlilamSuccess,
+  [BanpaidawarTypes.FETCHALLBANPAIDAWARLILAM_FAILURE]: fetchallbanpaidawarlilamFailure,
 
  
   [BanpaidawarTypes.LOCATIONS_REQUEST]: locationsRequest,
