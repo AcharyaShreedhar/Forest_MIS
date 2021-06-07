8e114f66d974652a58540ad1c7135288d361eb9c
Shreedhar Acharya <spacharya2537@gmail.com>
2021-06-07 12:05:44 +0545

    [FM-141]adding darta_no to samudayikban bibaran apis

--- server/app/controller/samudayikbanBibaranController.js
+++ server/app/controller/samudayikbanBibaranController.js
@@ -1,98 +1,102 @@
-const pool = require("../db")
+const pool = require("../db");
 //Controller for Listing all SamudayikbanBibaran
 async function getAllSamudayikbanBibaran(req, res) {
-    const getAllSamudayikbanBibaranQuery = `select * from samudayikban_bibarans`;
-    pool.query(getAllSamudayikbanBibaranQuery, [], (error, results, fields) => {
+  const getAllSamudayikbanBibaranQuery = `select * from samudayikban_bibarans`;
+  pool.query(getAllSamudayikbanBibaranQuery, [], (error, results, fields) => {
+    if (error) throw error;
+    res.send(JSON.stringify({ status: 200, error: null, data: results }));
+  });
+}
+
+//Controller for Listing a SamudayikbanBibaran
+async function getSamudayikbanBibaran(req, res) {
+  const getSamudayikbanBibaranQuery = `select * from samudayikban_bibarans where samudayikban_id=?`;
+  pool.query(
+    getSamudayikbanBibaranQuery,
+    [req.params.samudayikbanBibaranId],
+    (error, results, fields) => {
       if (error) throw error;
       res.send(JSON.stringify({ status: 200, error: null, data: results }));
-    });
-  }
-  
-  //Controller for Listing a SamudayikbanBibaran
-  async function getSamudayikbanBibaran(req, res) {
-    const getSamudayikbanBibaranQuery = `select * from samudayikban_bibarans where samudayikban_id=?`;
-    pool.query(getSamudayikbanBibaranQuery, [req.params.samudayikbanBibaranId], (error, results, fields) => {
-      if (error) throw error;
+    }
+  );
+}
+
+//Controller for adding a SamudayikbanBibaran
+async function addSamudayikbanBibaran(req, res) {
+  const addSamudayikbanBibaranQuery = `INSERT INTO samudayikban_bibarans (darta_no, samudayikban_name, area, main_species, forest_type, handover_date, forest_maujdat, nikasi_timber, nikasi_wood, created_by, updated_by) values (?,?,?,?,?,?,?,?,?,?,?)`;
+  pool.query(
+    addSamudayikbanBibaranQuery,
+    [
+      req.body.darta_no,
+      req.body.samudayikban_name,
+      req.body.area,
+      req.body.main_species,
+      req.body.forest_type,
+      req.body.handover_date,
+      req.body.forest_maujdat,
+      req.body.nikasi_timber,
+      req.body.nikasi_wood,
+      req.body.created_by,
+      req.body.updated_by,
+    ],
+    (error, results, fields) => {
+      if (error) {
+        throw error;
+      }
       res.send(JSON.stringify({ status: 200, error: null, data: results }));
-    });
-  }
-  
-  //Controller for adding a SamudayikbanBibaran
-  async function addSamudayikbanBibaran(req, res) {
-    const addSamudayikbanBibaranQuery = `INSERT INTO samudayikban_bibarans (samudayikban_name, area, main_species, forest_type, handover_date, forest_maujdat, nikasi_timber, nikasi_wood, created_by, updated_by) values (?,?,?,?,?,?,?,?,?,?)`;
-    pool.query(
-      addSamudayikbanBibaranQuery,
-      [
-        req.body.samudayikban_name,
-        req.body.area,
-        req.body.main_species,
-        req.body.forest_type,
-        req.body.handover_date,
-        req.body.forest_maujdat,
-        req.body.nikasi_timber,
-        req.body.nikasi_wood,
-        req.body.created_by,
-        req.body.updated_by,  
-      ],
-      (error, results, fields) => {
-        if (error) {
-          throw error;
-        }
-        res.send(JSON.stringify({ status: 200, error: null, data: results }));
-      }
-    );
-  }
-  
-  //Controller for updating a SamudayikbanBibaran
-  async function updateSamudayikbanBibaran(req, res) {
-    const updateSamudayikbanBibaranQuery = `UPDATE samudayikban_bibarans SET samudayikban_name=?, area=?, main_species=?, forest_type=?, handover_date=?, forest_maujdat=?, nikasi_timber=?, nikasi_wood=?, created_by=?, updated_by=? WHERE samudayikban_id=?`;
-    pool.query(
-      updateSamudayikbanBibaranQuery,
-      [
-        req.body.samudayikban_name,
-        req.body.area,
-        req.body.main_species,
-        req.body.forest_type,
-        req.body.handover_date,
-        req.body.forest_maujdat,
-        req.body.nikasi_timber,
-        req.body.nikasi_wood,
-        req.body.created_by,
-        req.body.updated_by,
-        req.params.samudayikbanBibaranId,
-      ],
-      (error, results, fields) => {
-        if (error) {
-          throw error;
-        }
-        res.send(JSON.stringify({ status: 200, error: null, data: results }));
-      }
-    );
-  }
-  
-  //Controller for deleting a SamudayikbanBibaran
-  async function deleteSamudayikbanBibaran(req, res) {
-    const deleteSamudayikbanBibaranQuery = `DELETE  FROM samudayikban_bibarans where samudayikban_id=?`;
-    pool.query(
-      deleteSamudayikbanBibaranQuery,
-      [req.params.samudayikbanBibaranId],
-      (error, results, fields) => {
-        if (error) {
-          throw error;
-        }
-        res.send(JSON.stringify({ status: 200, error: null, data: results }));
-      }
-    );
-  }
-  
-  module.exports = {
-    getAllSamudayikbanBibaran,
-    getSamudayikbanBibaran,
-    addSamudayikbanBibaran,
-    updateSamudayikbanBibaran,
-    deleteSamudayikbanBibaran,
-  };
+    }
+  );
+}
 
-  //** */
+//Controller for updating a SamudayikbanBibaran
+async function updateSamudayikbanBibaran(req, res) {
+  const updateSamudayikbanBibaranQuery = `UPDATE samudayikban_bibarans SET darta_no=?,samudayikban_name=?, area=?, main_species=?, forest_type=?, handover_date=?, forest_maujdat=?, nikasi_timber=?, nikasi_wood=?, created_by=?, updated_by=? WHERE samudayikban_id=?`;
+  pool.query(
+    updateSamudayikbanBibaranQuery,
+    [
+      req.body.darta_no,
+      req.body.samudayikban_name,
+      req.body.area,
+      req.body.main_species,
+      req.body.forest_type,
+      req.body.handover_date,
+      req.body.forest_maujdat,
+      req.body.nikasi_timber,
+      req.body.nikasi_wood,
+      req.body.created_by,
+      req.body.updated_by,
+      req.params.samudayikbanBibaranId,
+    ],
+    (error, results, fields) => {
+      if (error) {
+        throw error;
+      }
+      res.send(JSON.stringify({ status: 200, error: null, data: results }));
+    }
+  );
+}
 
-  
+//Controller for deleting a SamudayikbanBibaran
+async function deleteSamudayikbanBibaran(req, res) {
+  const deleteSamudayikbanBibaranQuery = `DELETE  FROM samudayikban_bibarans where samudayikban_id=?`;
+  pool.query(
+    deleteSamudayikbanBibaranQuery,
+    [req.params.samudayikbanBibaranId],
+    (error, results, fields) => {
+      if (error) {
+        throw error;
+      }
+      res.send(JSON.stringify({ status: 200, error: null, data: results }));
+    }
+  );
+}
+
+module.exports = {
+  getAllSamudayikbanBibaran,
+  getSamudayikbanBibaran,
+  addSamudayikbanBibaran,
+  updateSamudayikbanBibaran,
+  deleteSamudayikbanBibaran,
+};
+
+//** */
