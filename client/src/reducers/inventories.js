b86c254ac3eb77432e485066ec84f3ab558f0702
Ashish kc <kecyasis21@gmail.com>
2021-06-08 18:55:09 +0545

    [FM-147] code added for inventories in reducers

--- client/src/reducers/inventories.js
+++ client/src/reducers/inventories.js
@@ -21,26 +21,41 @@ const fetchallinventoriesFailure = (state, action) => {
   state.merge({ ...state, status: "error" });
 };
 
-const locationsRequest = (state, action) => {
-    let locations = state.locations;
-  
-    locations = prepend(action.payload.route, locations);
-    locations = dropLast(1, locations);
-    return state.merge({ ...state, locations });
-  };
-  
-  const clearRequest = (state, action) =>
-    state.merge({ ...state, ...initialState });
-  
-  export const reducer = createReducer(initialState, {
-    [InventoriesTypes.FETCHALLINVENTORIES_REQUEST]: fetchallinventoriesRequest,
-    [InventoriesTypes.FETCHALLINVENTORIES_SUCCESS]: fetchallinventoriesSuccess,
-    [InventoriesTypes.FETCHALLINVENTORIES_FAILURE]: fetchallinventoriesFailure,
-  
-   
-  
-  
-    [InventoriesTypes.LOCATIONS_REQUEST]: locationsRequest,
-    [InventoriesTypes.CLEAR_REQUEST]: clearRequest,
+const fetchinventoriesRequest = (state, action) =>
+  state.merge({ ...state, token: "", status: "pending" });
+const fetchinventoriesSuccess = (state, action) => {
+  return state.merge({
+    ...state,
+    status: "done",
+    inventoriesData: action.response,
   });
-  
+};
+const fetchinventoriesFailure = (state, action) => {
+  state.merge({ ...state, status: "error" });
+};
+
+
+const locationsRequest = (state, action) => {
+  let locations = state.locations;
+
+  locations = prepend(action.payload.route, locations);
+  locations = dropLast(1, locations);
+  return state.merge({ ...state, locations });
+};
+
+const clearRequest = (state, action) =>
+  state.merge({ ...state, ...initialState });
+
+export const reducer = createReducer(initialState, {
+  [InventoriesTypes.FETCHALLINVENTORIES_REQUEST]: fetchallinventoriesRequest,
+  [InventoriesTypes.FETCHALLINVENTORIES_SUCCESS]: fetchallinventoriesSuccess,
+  [InventoriesTypes.FETCHALLINVENTORIES_FAILURE]: fetchallinventoriesFailure,
+
+  [InventoriesTypes.FETCHINVENTORIES_REQUEST]: fetchinventoriesRequest,
+  [InventoriesTypes.FETCHINVENTORIES_SUCCESS]: fetchinventoriesSuccess,
+  [InventoriesTypes.FETCHINVENTORIES_FAILURE]: fetchinventoriesFailure,
+
+
+  [InventoriesTypes.LOCATIONS_REQUEST]: locationsRequest,
+  [InventoriesTypes.CLEAR_REQUEST]: clearRequest,
+});
