13c3be346168e7a6ef41b38b55926938ee0272b5
Ashish kc <kecyasis21@gmail.com>
2021-06-09 13:38:58 +0545

    [FM-98] code added for emplooyes in actions/karmacharibibaran.js

---
+++ client/src/actions/karmacharibibaran.js
@@ -0,0 +1,26 @@
+/**
+ * We will use the following prefixes.
+ * add: When adding something to the database.
+ * delete: When deleting something from the database.
+ * update: When updating something in the database.
+ * fetch: When pulling something from the database.
+ * search: When searching something in the database.
+ * select: When setting reducers, not handling with database, just handling only reducers.
+ */
+
+ import { createActions } from "reduxsauce";
+
+ const { Types, Creators } = createActions({
+   fetchallemployeesRequest: ["payload"],
+   fetchallemployeesSuccess: ["response"],
+   fetchallemployeesFailure: null,
+ 
+
+   locationsRequest: ["payload"],
+   // Clear all caches
+   clearRequest: null,
+ });
+ 
+ export const KarmacharibibaranTypes = Types;
+ export default Creators;
+ 
