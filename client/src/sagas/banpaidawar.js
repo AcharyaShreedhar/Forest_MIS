92005c841d330e7da35133a6ac34c49c2379b12d
Subash Paudel <subashpaudel144@gmail.com>
2021-06-10 12:12:56 +0545

    [FM-131] add update delete banpaidawar sagas

--- client/src/sagas/banpaidawar.js
+++ client/src/sagas/banpaidawar.js
@@ -1,5 +1,5 @@
 import { call, put } from "redux-saga/effects";
-
+import { toast } from "react-toastify";
 import { history } from "../reducers";
 import BanpaidawarActions from "../actions/banpaidawar";
 
@@ -27,6 +27,82 @@ export function* fetchbanpaidawarRequest(api, action) {
       yield put(BanpaidawarActions.fetchbanpaidawarFailure());
     }
   }
+
+
+  // Add banpaidawar
+export function* addbanpaidawarRequest(api, action) {
+  const { payload } = action;
+
+  const response = yield api.postBanpaidawarBanpaidawarAddNew(
+    payload.banpaidawar.data
+  );
+
+  if (response.ok) {
+    toast.success("सफलतापुर्वक वन पैदावार प्रविष्ट भयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchallbanpaidawarRequest(api);
+    yield call(history.push, "/forests/banpaidawarlist");
+    yield put(BanpaidawarActions.addbanpaidawarSuccess(response.data));
+  } else {
+    yield put(BanpaidawarActions.addbanpaidawarFailure());
+    toast.error("तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  }
+}
+
+
+// Update banpaidawar
+export function* updatebanpaidawarRequest(api, action) {
+  const { payload } = action;
+
+  const response = yield api.postBanpaidawarBanpaidawarUpdate(
+    payload.banpaidawar.data,
+    payload.id
+  );
+
+  if (response.ok) {
+    toast.success("सफलतापुर्वक वन पैदावार पुनः प्रविष्ट भयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchallbanpaidawarRequest(api);
+    yield call(history.push, "/forests/banpaidawarlist");
+    yield put(
+      BanpaidawarActions.updatebanpaidawarSuccess(response.data)
+    );
+  } else {
+    yield put(BanpaidawarActions.updatebanpaidawarFailure());
+    toast.error("तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  }
+}
+
+
+
+// Delete banpaidawar
+export function* deletebanpaidawarRequest(api, action) {
+  const { payload } = action;
+
+  const response = yield api.postBanpaidawarBanpaidawarDelete(payload);
+
+  if (response.ok) {
+    toast.success("सफलतापुर्वक वन पैदावार हटाईयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchallbanpaidawarRequest(api);
+    yield put(
+      BanpaidawarActions.deletebanpaidawarSuccess(response.data)
+    );
+  } else {
+    yield put(BanpaidawarActions.deletebanpaidawarFailure());
+    toast.error("तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  }
+}
+
 
   export function* fetchallbanpaidawarlilamRequest(api, action) {
  
