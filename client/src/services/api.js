b82454ee1df87c9fd72088646ae45df198a3a241
Ashish kc <kecyasis21@gmail.com>
2021-06-10 14:35:28 +0545

    [FM-98] code added for postid in services/api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -167,6 +167,8 @@ const create = (baseURL = Config.API_URL) => {
   const getLevelList = () => api.get("level");
   const getLevel =(levelId) => api.get(`level/${levelId}`);
   const getPostList = () => api.get("posts");
+  const getPost =(postId) => api.get(`posts/${postId}`);
+  
 
   return {
     loginByUsername,
@@ -237,6 +239,7 @@ const create = (baseURL = Config.API_URL) => {
     getLevelList,
     getLevel,
     getPostList,
+    getPost,
   };
 };
 export default {
