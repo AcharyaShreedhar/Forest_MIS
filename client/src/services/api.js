2be399cf505ffda93d8b7fb28bb40d2b6016bdc8
Ashish kc <kecyasis21@gmail.com>
2021-06-13 17:53:06 +0545

    [FM-98] code added for updateLevel in src/services/api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -372,6 +372,10 @@ const create = (baseURL = Config.API_URL) => {
   const postKarmacharibibaranLevelAddNew = (payload) =>
     api.post(`level`, payload);
 
+    //update
+  const postKarmacharibibaranLevelUpdate = (payload, levelId) =>
+  api.put(`employees/${levelId}`, payload);
+
   const getPostList = () => api.get("posts");
   const getPost = (postId) => api.get(`posts/${postId}`);
 
@@ -489,6 +493,7 @@ const create = (baseURL = Config.API_URL) => {
     getLevelList,
     getLevel,
     postKarmacharibibaranLevelAddNew,
+    postKarmacharibibaranLevelUpdate,
     getPostList,
     getPost,
 
