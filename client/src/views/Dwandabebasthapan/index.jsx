7867c21d6357dd58a5051132c67afc0520fcc643
Shreedhar Acharya <spacharya2537@gmail.com>
2021-06-16 15:45:56 +0545

    [FM-166]Manab banya jantu dwanda bebasthapan bibaran

---
+++ client/src/views/Dwandabebasthapan/index.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { isEmpty } from "ramda";
+import { NotFound } from "../../components";
+import dwandabebasthapanRoutes from "../../routes/dwandabebasthapan";
+
+export class Dwandabebasthapan extends Component {
+  componentDidMount() {
+    //function goes here
+  }
+
+  render() {
+    const { authenticated } = this.props;
+    return (
+      <Switch>
+        {dwandabebasthapanRoutes.map((prop, key) => {
+          console.log("activities routes", activitiesRoutes);
+          if (prop.redirect && authenticated) {
+            return <Redirect exact from={prop.path} to={prop.to} key={key} />;
+          }
+          if (prop.redirect && !authenticated) {
+            return (
+              <Route
+                exact
+                path={prop.path}
+                component={prop.component}
+                key={key}
+              />
+            );
+          }
+          if (!prop.redirect && prop.auth && !authenticated) {
+            return <Redirect exact from={prop.path} to="/" key={key} />;
+          }
+          return (
+            <Route
+              exact
+              path={prop.path}
+              component={prop.component}
+              key={key}
+            />
+          );
+        })}
+        <Route path="*" exact component={NotFound} />
+      </Switch>
+    );
+  }
+}
+
+Dwandabebasthapan.propTypes = {
+  authenticated: PropTypes.bool.isRequired,
+  history: PropTypes.any,
+};
+
+Dwandabebasthapan.defaultProps = {
+  authenticated: false,
+  history: () => {},
+};
+
+const mapStateToProps = (state) => ({
+  //   role: state.app.app_role_id,
+  //   authenticated: !isEmpty(state.app.token),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  //function
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dwandabebasthapan);
