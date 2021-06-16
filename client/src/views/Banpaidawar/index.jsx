fe01703e7f5f242f9a9bfebaf4fa80259e09dde9
Subash Paudel <subashpaudel144@gmail.com>
2021-06-16 17:56:12 +0545

    [FM-176] list banpaidawarlilam banpaidawar/index

--- client/src/views/Banpaidawar/index.jsx
+++ client/src/views/Banpaidawar/index.jsx
@@ -3,20 +3,25 @@ import { PropTypes } from "prop-types";
 import { connect } from "react-redux";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { NotFound } from "../../components";
+import { isEmpty } from "ramda";
 import banpaidawarRoutes from "../../routes/banpaidawar";
+import BanpaidawarActions from "../../actions/banpaidawar";
 
 export class Banpaidawar extends Component {
   componentDidMount() {
-    //function to fetch
+    this.props.fetchallBanpaidawarlilam();
   }
 
   render() {
+
+    const { authenticated } = this.props;
     return (
       <Switch>
         {banpaidawarRoutes.map((prop, key) => {
-          if (prop.redirect) {
+          if (prop.redirect && authenticated) {
             return <Redirect exact from={prop.path} to={prop.to} key={key} />;
           }
+          if (prop.redirect && authenticated){
           return (
             <Route
               exact
@@ -25,15 +30,44 @@ export class Banpaidawar extends Component {
               key={key}
             />
           );
-        })}
+        }
+        if(!prop.redirect && prop.auth && !authenticated){
+          return <Redirect exact from = { prop.path} to="/" key={key} />;
+        }
+        return (
+          <Route
+              exact
+              path={prop.path}
+              component={prop.component}
+              key={key}
+          />
+        );
+        }
+        )}
         <Route path="*" exact component={NotFound} />
       </Switch>
     );
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  //function
+Banpaidawar.propTypes = {
+  authenticated: PropTypes.bool.isRequired,
+  history: PropTypes.any,
+};
+
+Banpaidawar.defaultProps = {
+  authenticated: false,
+  history: () => {},
+};
+
+const mapStateToProps = (state) => ({
+  role: state.app.app_role_id,
+  authenticated: !isEmpty(state.app.token),
 });
 
-export default connect(null, mapDispatchToProps)(Banpaidawar);
+const mapDispatchToProps = (dispatch) => ({
+  fetchallBanpaidawarlilam: () =>
+  dispatch(BanpaidawarActions.fetchallbanpaidawarlilamRequest()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Banpaidawar);
