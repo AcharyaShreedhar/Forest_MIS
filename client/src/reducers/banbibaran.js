414cfd91518479cd9f3a4f77e16cce626a70f754
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-06-20 15:35:40 +0545

    [FM-94]reducers update1 baramaditchijbastu_banbibaran_get

--- client/src/reducers/banbibaran.js
+++ client/src/reducers/banbibaran.js
@@ -11,6 +11,7 @@ const initialState = Immutable({
 const fetchallbaramaditchijbastuRequest = (state, action) =>
   state.merge({ ...state, token: "", status: "pending" });
 const fetchallbaramaditchijbastuSuccess = (state, action) => {
+  console.log("reducer", action.response);
   return state.merge({
     ...state,
     status: "done",
@@ -21,21 +22,39 @@ const fetchallbaramaditchijbastuFailure = (state, action) => {
   state.merge({ ...state, status: "error" });
 };
 
+const fetchbaramaditchijbastuRequest = (state, action) =>
+  state.merge({ ...state, token: "", status: "pending" });
+
+const fetchbaramaditchijbastuSuccess = (state, action) => {
+  return state.merge({
+    ...state,
+    status: "done",
+    baramaditchijbastuData: action.response,
+  });
+};
+const fetchbaramaditchijbastuFailure = (state, action) => {
+  state.merge({ ...state, status: "error" });
+};
+  
 const locationsRequest = (state, action) => {
-    let locations = state.locations;
-  
-    locations = prepend(action.payload.route, locations);
-    locations = dropLast(1, locations);
-    return state.merge({ ...state, locations });
-  };
-  
-  const clearRequest = (state, action) =>
-    state.merge({ ...state, ...initialState });
+  let locations = state.locations;
+
+  locations = prepend(action.payload.route, locations);
+  locations = dropLast(1, locations);
+  return state.merge({ ...state, locations });
+};
+
+const clearRequest = (state, action) =>
+  state.merge({ ...state, ...initialState });
 
     export const reducer = createReducer(initialState, {
         [BanbibaranTypes.FETCHALLBARAMADITCHIJBASTU_REQUEST]: fetchallbaramaditchijbastuRequest,
         [BanbibaranTypes.FETCHALLBARAMADITCHIJBASTU_SUCCESS]: fetchallbaramaditchijbastuSuccess,
         [BanbibaranTypes.FETCHALLBARAMADITCHIJBASTU_FAILURE]: fetchallbaramaditchijbastuFailure, 
+
+        [BanbibaranTypes.FETCHBARAMADITCHIJBASTU_REQUEST]: fetchbaramaditchijbastuRequest,
+        [BanbibaranTypes.FETCHBARAMADITCHIJBASTU_SUCCESS]: fetchbaramaditchijbastuSuccess,
+        [BanbibaranTypes.FETCHBARAMADITCHIJBASTU_FAILURE]: fetchbaramaditchijbastuFailure,
 
         [BanbibaranTypes.LOCATIONS_REQUEST]: locationsRequest,
         [BanbibaranTypes.CLEAR_REQUEST]: clearRequest,
