e26f37be77bc9e1ca13dd390e0aadaa251888537
anilakheli <kanxoakheli22@gmail.com>
2021-06-22 13:38:52 +0545

    [fm-181] changing method on bandadelobibaran

--- server/app/routes/bandadeloBibaranRoutes.js
+++ server/app/routes/bandadeloBibaranRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const bandadeloBibaranController = require("../controller/bandadeloBibaranController");
-router.get("/bandadeloBibaran", bandadeloBibaranController.getAllBandadeloBibaran);
+router.post("/bandadeloBibaranList", bandadeloBibaranController.getAllBandadeloBibaran);
 router.get("/bandadeloBibaran/:bandadeloBibaranId", bandadeloBibaranController.getBandadeloBibaran);
 router.post("/bandadeloBibaran", bandadeloBibaranController.addBandadeloBibaran);
 router.put("/bandadeloBibaran/:bandadeloBibaranId", bandadeloBibaranController.updateBandadeloBibaran);
