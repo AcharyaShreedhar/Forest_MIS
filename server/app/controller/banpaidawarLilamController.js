55b03c89fac71f95e31847fd694c0a91f24db4f2
anilakheli <kanxoakheli22@gmail.com>
2021-06-22 17:31:09 +0545

    adding pagination on banpaidawar

--- server/app/controller/banpaidawarLilamController.js
+++ server/app/controller/banpaidawarLilamController.js
@@ -1,10 +1,27 @@
 const pool = require("../db")
 //Controller for Listing all activities_infos
 async function getAllBanpaidawarLilam(req, res) {
-    const getAllBanpaidawarLilamQuery = `select * from banpaidawar_lilams`;
-    pool.query(getAllBanpaidawarLilamQuery, [], (error, results, fields) => {
+  const getTotalQuery = "SELECT count(*) as total from banpaidawar_lilams";
+    const getAllBanpaidawarLilamQuery = `select * from banpaidawar_lilams ORDER BY ? ASC LIMIT ?, ?`;
+    pool.query(getTotalQuery, [], (error, countresults, fields) => {
       if (error) throw error;
-      res.send(JSON.stringify({ status: 200, error: null, data: results }));
+      pool.query(
+        getAllBanpaidawarLilamQuery,
+        [req.body.name, req.body.page, req.body.perPage],
+        (error, results, fields) => {
+          if (error) throw error;
+          res.send(
+            JSON.stringify({
+              status: 200,
+              error: null,
+              data: {
+                total: countresults[0].total,
+                list: results,
+              },
+            })
+          );
+        }
+      );
     });
   }
   
