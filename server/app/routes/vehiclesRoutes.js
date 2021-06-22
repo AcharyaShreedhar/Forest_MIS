2d64eff8f1b6c407da8ba1615bf81a95efad760e
anilakheli <kanxoakheli22@gmail.com>
2021-06-22 17:00:25 +0545

    fm-181 changing method

--- server/app/routes/vehiclesRoutes.js
+++ server/app/routes/vehiclesRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const vehiclesController = require("../controller/vehiclesController");
-router.get("/vehicles", vehiclesController.getAllVehicles);
+router.post("/vehiclesList", vehiclesController.getAllVehicles);
 router.get("/vehicles/:vehicleId", vehiclesController.getVehicles);
 router.post("/vehicles", vehiclesController.addVehicles);
 router.put("/vehicles/:vehicleId", vehiclesController.updateVehicles);
