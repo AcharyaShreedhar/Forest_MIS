bfc56249740e0e02f56133e2cd073279a0c8159e
Subash Paudel <subashpaudel144@gmail.com>
2021-06-22 21:04:33 +0545

    [FM-164] list banxetra atikraman banxetraatikraman/index pagination

--- client/src/views/Banbibaran/Banxetraatikraman/index.jsx
+++ client/src/views/Banbibaran/Banxetraatikraman/index.jsx
@@ -1,13 +1,158 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
+import { equals, length, isNil } from "ramda";
+import { BanxetraAtikraman } from "../../../components";
+import BanxetraatikramanActions from "../../../actions/bandadelobibaran";
+import { banxetraatikramanHeadings } from "../../../services/config";
 
-export class index extends Component {
-    render() {
-        return (
-            <div>
-                banxetraatikramanlist
-            </div>
-        )
+
+class Banxetraatikraman extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loc: "banxetraatikramanlist" };
+    this.handleSelectMenu = this.handleSelectMenu.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+      console.log("Next props", nextProps);
+    const loc = nextProps.location.pathname.split("/")[2];
+    var banxetraatikramanList = [];
+    if (nextProps != prevState) {
+        banxetraatikramanList = nextProps.banxetraatikramanDataList.data;
     }
+
+    return {
+        loc,
+        banxetraatikramanList
+     };
+  }
+
+  handlePageChange(data, item) {
+      const {perPage } = this.state;
+      this.setState({ page: data.selected });
+      this.props.fetchallBanxetraatikraman({
+          name:"address",
+          page: data.selected * perPage,
+          perPage,
+      });
+  }
+
+  handleSelectMenu(event, item, path) {
+   switch (event) {
+      case "edit": {
+        switch (path) {
+          case "banxetraatikraman": {
+            this.props.history.push({
+              pathname: `/banbibaran/banxetraatikramanedit/${item.banxetra_atikraman_id}`,
+              item,
+            });
+            break;
+          }
+          default:
+            break;
+        }
+        break;
+      }
+      case "delete": {
+        switch (path) {
+          case "banxetraatikraman": {
+            this.props.deleteBanxetraatikraman(item.banxetra_atikraman_id);
+            break;
+          }
+          default:
+            break;
+        }
+      }
+      default:
+        break;
+    }
+  }
+
+  handleAdd(item) {
+    switch (item) {
+      case "banxetraatikraman": {
+        this.props.history.push("/banbibaran/banxetraatikramanadd/new");
+        break;
+      }
+
+      default:
+        break;
+    }
+  }
+
+  render() {
+    const {
+        loc,
+        perPage,
+        banxetraatikramanList,
+    } = this.state;
+    const { user } = this.props;
+       
+    return (
+      <div>
+        {equals(loc, "banxetraatikramanlist") && (
+          <BanxetraAtikraman.List
+            buttonName="+ वनक्षेत्र अतिक्रमण"
+            title="वनक्षेत्र अतिक्रमण सम्बन्धि विवरण"
+            pageCount={
+                !isNil(banxetraatikramanList)
+                  ? Math.ceil(banxetraatikramanList.total / perPage)
+                  : 10
+              }
+            data={ !isNil(banxetraatikramanList) ? banxetraatikramanList.list : []}
+            headings={banxetraatikramanHeadings}
+            user={user}
+            onAdd={() => this.handleAdd("banxetraatikraman")}
+            onSelect={this.handleSelectMenu}
+            onPageClick={(e) => this.handlePageChange(e, "banxetraatikraman")}
+          />
+        )}
+        {equals(loc, "banxetraatikramanadd") && (
+          <BanxetraAtikraman.Add
+            title="+ वनक्षेत्र अतिक्रमण"
+            onSelect={this.handleSelectMenu}
+            onSubmit={(e) => this.props.addBanxetraatikraman (e)}
+          />
+        )}
+        {equals(loc, "banxetraatikramanedit") && (
+          <BanxetraAtikraman.Edit
+            title="वनक्षेत्र अतिक्रमण पुनः प्रविष्ट"
+            history={this.props.history}
+            onSelect={this.handleSelectMenu}
+            onUpdate={(e, id) => this.props.updateBandadelo(e, id)}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
-export default index
+Banxetraatikraman.propsTypes = {
+  banxetraatikramanDataList: PropTypes.any,
+};
+
+Banxetraatikraman.defaultProps = {
+  banxetraatikramanDataList: {},
+};
+
+const mapStateToProps = (state) => ({
+  banxetraatikramanDataList: state.banxetraatikraman.allbanxetraatikramanData,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+  addBanxetraatikraman: (payload) =>
+    dispatch(BanxetraatikramanActions.addbanxetraatikramanRequest(payload)),
+
+  updateBanxetraatikraman: (payload, banxetraatikramanId) =>
+    dispatch(
+      BanxetraatikramanActions.updatebanxetraatikramanRequest(payload, banxetraatikramanId)
+    ),
+
+  deleteBanxetraatikraman: (banxetraatikramanId) =>
+    dispatch(BanxetraatikramanActions.deletebanxetraatikramanRequest(banxetraatikramanId)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Banxetraatikraman);
