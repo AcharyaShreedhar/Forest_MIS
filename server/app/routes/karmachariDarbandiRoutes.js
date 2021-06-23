bf43498a12a40561864bde8487cff2226dfa1819
anilakheli <kanxoakheli22@gmail.com>
2021-06-23 12:51:14 +0545

    adding parameter and changing request

--- server/app/routes/karmachariDarbandiRoutes.js
+++ server/app/routes/karmachariDarbandiRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const karmachariDarbandiController = require("../controller/karmachariDarbandiController");
-router.get("/karmachariDarbandi", karmachariDarbandiController.getAllKarmachariDarbandi);
+router.post("/karmachariDarbandiList", karmachariDarbandiController.getAllKarmachariDarbandi);
 router.get("/karmachariDarbandi/:karmachariDarbandiId", karmachariDarbandiController.getKarmachariDarbandi);
 router.post("/karmachariDarbandi", karmachariDarbandiController.addKarmachariDarbandi);
 router.put("/karmachariDarbandi/:karmachariDarbandiId", karmachariDarbandiController.updateKarmachariDarbandi);
