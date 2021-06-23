1dd95701583c45ed5b0e3c9fed602dbdf92ed143
anilakheli <kanxoakheli22@gmail.com>
2021-06-23 09:48:06 +0545

    adding parameter and changing request

--- server/app/routes/employeeHistoryRoutes.js
+++ server/app/routes/employeeHistoryRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const employeeHistoryController = require("../controller/employeeHistoryController");
-router.get("/employeeHistory", employeeHistoryController.getAllEmployeeHistory);
+router.post("/employeeHistoryList", employeeHistoryController.getAllEmployeeHistory);
 router.get("/employeeHistory/:histId", employeeHistoryController.getEmployeeHistory);
 router.post("/employeeHistory", employeeHistoryController.addEmployeeHistory);
 router.put("/employeeHistory/:histId", employeeHistoryController.updateEmployeeHistory);
