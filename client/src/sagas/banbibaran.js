7d04683b6204b8ff451749e7ea70913d772b64f1
anilakheli <kanxoakheli22@gmail.com>
2021-06-24 12:48:02 +0545

    adding request on baramaditchijbastu

--- client/src/sagas/banbibaran.js
+++ client/src/sagas/banbibaran.js
@@ -28,6 +28,35 @@ export function* fetchbaramaditchijbastuRequest(api, action) {
   }
 }
 
+export function* addbaramaditchijbastuRequest(api, action) {
+  const { payload } = action;
+
+  const response = yield api.postBaramaditchijbastuAddnew(
+    payload.baramaditchijbastu.data
+  );
+
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बरामदितचिज बस्तु प्रविष्ट भयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchallbaramaditchijbastuRequest(api, {
+      name: "established_date",
+      page: 0,
+      perPage: 10,
+    });
+    yield call(history.push, "/banbibaran/baramaditChijbastu");
+    yield put(BanbibaranActions.addbaramaditchijbastuSuccess(response.data));
+  } else {
+    yield put(BanbibaranActions.addbaramaditchijbastuFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
+  }
+}
+
 export function* fetchallbanxetraanyaprayojanRequest(api, action) {
   const { payload } = action;
   const payloaddata = isNil(payload) ? action : payload;
