f546e6adccecc97910eb3ebd4d6c99f9a2eab62f
Subash Paudel <subashpaudel144@gmail.com>
2021-06-24 14:27:03 +0545

    [FM-163] services mistake modified

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -262,8 +262,8 @@ const create = (baseURL = Config.API_URL) => {
 
   //delete muddaanusandhandayari
   const postMuddaanusandhandayariMuddaanusandhandayariDelete = (
-    muddaAnusandhandayariId
-  ) => api.delete(`muddaAnusandhanDayari/${muddaAnusandhandayariId}`);
+    muddaanusandhandayariId
+  ) => api.delete(`muddaAnusandhanDayaris/${muddaanusandhandayariId}`);
 
   // ------banxetraatikraman
   const getBanxetraatikramanList = (payload) => api.post("banxetraAtikramansList",payload);
