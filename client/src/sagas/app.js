3488a5b1d52e8dc14fc43170c95e84f0b6c16523
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-06-25 17:56:58 +0545

    [FM-180]sagas update_pagination app

--- client/src/sagas/app.js
+++ client/src/sagas/app.js
@@ -44,7 +44,91 @@ export function* fetchmunicipalitiesRequest(api, action) {
     yield put(AppActions.fetchmunicipalitiesFailure());
   }
 }
-
+// Add municipalities
+export function* addmunicipalitiesRequest(api, action) {
+  const { payload } = action;
+ 
+  const response = yield api.postMunicipalitiesAddNew(
+    payload.plotbibaran.data
+  );
+ 
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बगैंचा विवरण प्रविष्ट भयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchallmunicipalitiesRequest(api, {
+      name: "mun_name_nep",
+      page: 0,
+      perPage: 10,
+    });
+    yield call(history.push, "/app/municipalitieslist");
+    yield put(BanbibaranActions.addplotbibaranSuccess(response.data));
+  } else {
+    yield put(BanbibaranActions.addplotbibaranFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
+  }
+}
+ 
+// Update municipalities
+export function* updatemunicipalitiesRequest(api, action) {
+  const { payload, plotId } = action;
+ 
+  const response = yield api.postMunicipalitiesUpdate(
+    payload.municipalities.data,
+    municipalitiesId
+  );
+ 
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बगैंचा विवरण पुनः प्रविष्ट भयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchallmunicipalitiesRequest(api, {
+      name: "mun_name_nep",
+      page: 0,
+      perPage: 10,
+    });
+    yield call(history.push, "/app/municipalitieslist");
+    yield put(AppActions.updatemunicipalitiesSuccess(response.data));
+  } else {
+    yield put(AppActions.updatemunicipalitiesFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
+  }
+}
+ 
+// Delete municipalities
+export function* deletemunicipalitiesRequest(api, action) {
+  const { payload } = action;
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बगैंचा विवरण हटाईयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchallplotbibaranRequest(api, {
+      name: "mun_name_nep",
+      page: 0,
+      perPage: 10,
+    });
+    yield put(AppActions.deleteplotbibaranSuccess(response.data));
+  } else {
+    yield put(AppActions.deleteplotbibaranFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
+  }
+}
+ 
 // Provinces
 export function* fetchallprovincesRequest(api, action) {
   const { payload } = action;
@@ -67,6 +151,94 @@ export function* fetchprovincesRequest(api, action) {
     yield put(AppActions.fetchprovincesFailure());
   }
 }
+// Add provinces
+export function* addprovincesRequest(api, action) {
+  const { payload } = action;
+ 
+  const response = yield api.postProvincesAddNew(
+    payload.provinces.data
+  );
+ 
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बगैंचा विवरण प्रविष्ट भयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchallprovincesRequest(api, {
+      name: "prov_name_nep",
+      page: 0,
+      perPage: 10,
+    });
+    yield call(history.push, "/app/provinceslist");
+    yield put(AppActions.addprovincesSuccess(response.data));
+  } else {
+    yield put(AppActions.addprovincesFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
+  }
+}
+ 
+// Update provinces
+export function* updateprovincesRequest(api, action) {
+  const { payload, provincesId } = action;
+ 
+  const response = yield api.postProvincesUpdate(
+    payload.provinces.data,
+    provincesId
+  );
+ 
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बगैंचा विवरण पुनः प्रविष्ट भयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchallprovincesRequest(api, {
+      name: "prov_name_nep",
+      page: 0,
+      perPage: 10,
+    });
+    yield call(history.push, "/app/provinceslist");
+    yield put(AppActions.updateprovincesSuccess(response.data));
+  } else {
+    yield put(AppActions.updateprovincesFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
+  }
+}
+ 
+// Delete provinces
+export function* deleteprovincesRequest(api, action) {
+  const { payload } = action;
+ 
+  const response = yield api.postProvincesDelete(payload);
+ 
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बगैंचा विवरण हटाईयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchallprovincesRequest(api, {
+      name: "prov_name_nep",
+      page: 0,
+      perPage: 10,
+    });
+    yield put(AppActions.deleteprovincesSuccess(response.data));
+  } else {
+    yield put(AppActions.deleteprovincesFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
+  }
+}
+ 
 
 // Districts
 export function* fetchalldistrictsRequest(api, action) {
@@ -88,6 +260,93 @@ export function* fetchdistrictsRequest(api, action) {
     yield put(AppActions.fetchdistrictsSuccess(response.data));
   } else {
     yield put(AppActions.fetchdistrictsFailure());
+  }
+}
+// Add districts
+export function* adddistrictsRequest(api, action) {
+  const { payload } = action;
+ 
+  const response = yield api.postDistrictsAddNew(
+    payload.districts.data
+  );
+ 
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बगैंचा विवरण प्रविष्ट भयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchalldistrictsRequest(api, {
+      name: "dist_name_nep",
+      page: 0,
+      perPage: 10,
+    });
+    yield call(history.push, "/app/districtslist");
+    yield put(AppActions.adddistrictsSuccess(response.data));
+  } else {
+    yield put(AppActions.adddistrictsFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
+  }
+}
+ 
+// Update districts
+export function* updatedistrictsRequest(api, action) {
+  const { payload, districtsId } = action;
+ 
+  const response = yield api.postDistrictsUpdate(
+    payload.districts.data,
+    districtsId
+  );
+ 
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बगैंचा विवरण पुनः प्रविष्ट भयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchalldistrictsRequest(api, {
+      name: "dist_name_nep",
+      page: 0,
+      perPage: 10,
+    });
+    yield call(history.push, "/app/districtslist");
+    yield put(AppActions.updatedistrictsSuccess(response.data));
+  } else {
+    yield put(AppActions.updatedistrictsFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
+  }
+}
+ 
+// Delete districts
+export function* deletedistrictsRequest(api, action) {
+  const { payload } = action;
+ 
+  const response = yield api.postDistrictsDelete(payload);
+ 
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बगैंचा विवरण हटाईयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchallplotbibaranRequest(api, {
+      name: "dist_name_nep",
+      page: 0,
+      perPage: 10,
+    });
+    yield put(AppActions.deletedistrictsSuccess(response.data));
+  } else {
+    yield put(AppActions.deletedistrictsFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
   }
 }
 
@@ -113,6 +372,95 @@ export function* fetchdepartmentsRequest(api, action) {
     yield put(AppActions.fetchdepartmentsFailure());
   }
 }
+// Add departments
+export function* adddepartmentsRequest(api, action) {
+  const { payload } = action;
+ 
+  const response = yield api.postdepartmentsAddNew(
+    payload.departments.data
+  );
+ 
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बगैंचा विवरण प्रविष्ट भयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchalldepartmentsRequest(api, {
+      name: "dept_name_nep",
+      page: 0,
+      perPage: 10,
+    });
+    yield call(history.push, "/app/departmentslist");
+    yield put(AppActions.adddepartmentsSuccess(response.data));
+  } else {
+    yield put(AppActions.adddepartmentsFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
+  }
+}
+ 
+// Update departments
+export function* updatedepartmentsRequest(api, action) {
+  const { payload, departmentsId } = action;
+ 
+  const response = yield api.postdepartmentsUpdate(
+    payload.departments.data,
+    departmentsId
+  );
+ 
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बगैंचा विवरण पुनः प्रविष्ट भयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchalldepartmentsRequest(api, {
+      name: "dept_name_nep",
+      page: 0,
+      perPage: 10,
+    });
+    yield call(history.push, "/app/departmentslist");
+    yield put(AppActions.updatedepartmentsSuccess(response.data));
+  } else {
+    yield put(AppActions.updatedepartmentsFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
+  }
+}
+ 
+// Delete departments
+export function* deletedepartmentsRequest(api, action) {
+  const { payload } = action;
+ 
+  const response = yield api.postDepartmentsDelete(payload);
+ 
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बगैंचा विवरण हटाईयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchalldepartmentsRequest(api, {
+      name: "dept_name_nep",
+      page: 0,
+      perPage: 10,
+    });
+    yield put(AppActions.deletedepartmentsSuccess(response.data));
+  } else {
+    yield put(AppActions.deletedepartmentsFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
+  }
+}
+ 
+
 
 // Users
 export function* fetchallusersRequest(api, action) {
@@ -137,6 +485,96 @@ export function* fetchusersRequest(api, action) {
     yield put(AppActions.fetchusersFailure());
   }
 }
+
+// Add users
+export function* addusersRequest(api, action) {
+  const { payload } = action;
+ 
+  const response = yield api.postusersAddNew(
+    payload.users.data
+  );
+ 
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बगैंचा विवरण प्रविष्ट भयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchallusersRequest(api, {
+      name: "user_name",
+      page: 0,
+      perPage: 10,
+    });
+    yield call(history.push, "/app/departmentslist");
+    yield put(AppActions.addusersSuccess(response.data));
+  } else {
+    yield put(AppActions.adddusersFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
+  }
+}
+ 
+// Update users
+export function* updateusersRequest(api, action) {
+  const { payload, usersId } = action;
+ 
+  const response = yield api.postusersUpdate(
+    payload.departments.data,
+    usersId
+  );
+ 
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बगैंचा विवरण पुनः प्रविष्ट भयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchallusersRequest(api, {
+      name: "user_name",
+      page: 0,
+      perPage: 10,
+    });
+    yield call(history.push, "/app/departmentslist");
+    yield put(AppActions.updateusersSuccess(response.data));
+  } else {
+    yield put(AppActions.usersFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
+  }
+}
+ 
+// Delete users
+export function* deleteusersRequest(api, action) {
+  const { payload } = action;
+ 
+  const response = yield api.postUsersDelete(payload);
+ 
+  if (response.ok) {
+    toast.success("सफलतापुर्वक बगैंचा विवरण हटाईयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield fetchallusersRequest(api, {
+      name: "user_name",
+      page: 0,
+      perPage: 10,
+    });
+    yield put(AppActions.deleteusersSuccess(response.data));
+  } else {
+    yield put(AppActions.deleteusersFailure());
+    toast.error(
+      "तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!",
+      {
+        position: toast.POSITION.TOP_CENTER,
+      }
+    );
+  }
+}
+ 
+
 export function* logoutRequest(api, action) {
   yield put(AppActions.clearRequest());
   yield put(AppActions.logoutSuccess());
