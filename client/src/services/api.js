3440e494b4d5791bece3a16c77bc728944b7e2e7
Ashish kc <kecyasis21@gmail.com>
2021-06-27 13:49:03 +0545

    [FM-193] code added for Uddhyam bibaranList in src/services/api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -166,6 +166,10 @@ const create = (baseURL = Config.API_URL) => {
     const postBanbibaranBanxetraanyaprayojanDelete = (payload, banxetraanyaprayojanId
       ) => api.delete(`banxetraanyaprayojan`, payload);
 
+    //UdhhyamBibarans
+    const getUddhyambibaranList = (payload) =>
+    api.post("uddhyamBibaranList", payload);
+
   //................biruwautpadans
   const getBiruwautpadanList = (payload) => api.post("biruwaUtpadansList", payload);
 
@@ -704,6 +708,7 @@ const create = (baseURL = Config.API_URL) => {
     postBanbibaranBanxetraanyaprayojanAddNew,
     postBanbibaranBanxetraanyaprayojanUpdate,
     postBanbibaranBanxetraanyaprayojanDelete,
+    getUddhyambibaranList,
   };
 };
 export default {
