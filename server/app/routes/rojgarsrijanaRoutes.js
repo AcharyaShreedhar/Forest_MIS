c9d69570918e6770e5b9182c44861e9b209a1547
anilakheli <kanxoakheli22@gmail.com>
2021-06-28 17:31:54 +0545

    spelling correction on rojgarsrijanaroutes

--- server/app/routes/rojgarsrijanaRoutes.js
+++ server/app/routes/rojgarsrijanaRoutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const rojgarsrijanaController = require("../controller/rojgarsrijanaController");
-router.post("/rogjarsrijanaList", rojgarsrijanaController.getAllRojgarSrijana);
-router.get("/rogjarsrijana/:rojgarsrijanaId", rojgarsrijanaController.getRojgarSrijana);
-router.post("/rogjarsrijana", rojgarsrijanaController.addRojgarSrijana);
-router.put("/rogjarsrijana/:rojgarsrijanaId", rojgarsrijanaController.updateRojgarSrijan);
-router.delete("/rogjarsrijana/:rojgarsrijanaId", rojgarsrijanaController.deleteRojgarSrijana);
+router.post("/rojgarsrijanaList", rojgarsrijanaController.getAllRojgarSrijana);
+router.get("/rojgarsrijana/:rojgarsrijanaId", rojgarsrijanaController.getRojgarSrijana);
+router.post("/rojgarsrijana", rojgarsrijanaController.addRojgarSrijana);
+router.put("/rojgarsrijana/:rojgarsrijanaId", rojgarsrijanaController.updateRojgarSrijan);
+router.delete("/rojgarsrijana/:rojgarsrijanaId", rojgarsrijanaController.deleteRojgarSrijana);
 module.exports = router;
