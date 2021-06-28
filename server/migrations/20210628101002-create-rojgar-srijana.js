870c6d2c15743088a1460876945373a5662fc999
anilakheli <kanxoakheli22@gmail.com>
2021-06-28 16:02:29 +0545

    adding mmigration model for banle srijana gareko rojgar

---
+++ server/migrations/20210628101002-create-rojgar-srijana.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('rojgar_srijanas', {
+      rojgar_srijana_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      karyaharu: {
+        type: Sequelize.STRING
+      },
+      ekai :{
+        type: Sequelize.STRING,
+      },
+      banka_prakar:{
+        type: Sequelize.STRING,
+      },
+      mahila:{
+        type:Sequelize.STRING,
+      },
+      purus:{
+        type:Sequelize.STRING,
+      },
+      jamma:{
+        type:Sequelize.STRING,
+      },
+      kaifiyat:{
+        type:Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('rojgar_srijanas');
+  }
+};
