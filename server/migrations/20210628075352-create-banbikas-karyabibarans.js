7f8f93c07a7b9eceaad6a7a8a75a1fe68410a0ed
Subash Paudel <subashpaudel144@gmail.com>
2021-06-28 13:51:14 +0545

    [FM-199] migration and model for banbikas_karyabibarans

---
+++ server/migrations/20210628075352-create-banbikas-karyabibarans.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('banbikas_karyabibarans', {
+      banbikas_karyabibaran_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      banbikas_karyabibaran: {
+        type: Sequelize.STRING
+      },
+      banbikas_ikai: {
+        type: Sequelize.STRING
+      },
+      banbikas_parinam: {
+        type: Sequelize.STRING
+      },
+      banbikas_bajetkharcha: {
+        type: Sequelize.STRING
+      },
+      ban_type: {
+        type: Sequelize.STRING
+      },
+      created_by: {
+        type: Sequelize.STRING
+      },
+      updated_by: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('banbikas_karyabibarans');
+  }
+};
