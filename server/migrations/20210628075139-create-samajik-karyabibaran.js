ec7d7cd742cede54a29a51fd49cb9cfd9dad16fa
Ashish kc <kecyasis21@gmail.com>
2021-06-28 13:47:53 +0545

    [FM-200] samajik karyabibaran migration added

---
+++ server/migrations/20210628075139-create-samajik-karyabibaran.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('samajik_karyabibarans', {
+      samajik_karyabibaran_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      samajik_karyabibaran: {
+        type: Sequelize.STRING
+      },
+      samajik_ekai: {
+        type: Sequelize.STRING
+      },
+      samajik_parinam: {
+        type: Sequelize.STRING
+      },
+      samajik_bajetkharcha: {
+        type: Sequelize.STRING
+      },
+      ban_type: {
+        type: Sequelize.STRING
+      },
+      created_by: {
+        type: Sequelize.STRING
+      },
+      updated_by: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('samajik_karyabibarans');
+  }
+};
