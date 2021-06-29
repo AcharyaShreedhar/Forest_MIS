e369ac48b17356bb8451fb04fa9171a4d485643b
Ashish kc <kecyasis21@gmail.com>
2021-06-29 16:43:17 +0545

    [FM-202] Code added for SamajikkaryabibaranList in reducers

---
+++ client/src/reducers/karyabibaran.js
@@ -0,0 +1,43 @@
+import { createReducer } from "reduxsauce";
+import Immutable from "seamless-immutable";
+import { dropLast, prepend } from "ramda";
+import {KaryabibaranTypes } from "../actions/karyabibaran";
+
+const initialState = Immutable({
+  status: "",
+});
+
+const fetchallsamajikkaryabibaranRequest = (state, action) =>
+  state.merge({ ...state, status: "pending" });
+const fetchallsamajikkaryabibaranSuccess = (state, action) => {
+
+  return state.merge({
+    ...state,
+    status: "done",
+    allsamajikkaryabibaranData: action.response,
+  });
+};
+const fetchallsamajikkaryabibaranFailure = (state, action) => {
+  state.merge({ ...state, status: "error" });
+};
+
+const locationsRequest = (state, action) => {
+    let locations = state.locations;
+  
+    locations = prepend(action.payload.route, locations);
+    locations = dropLast(1, locations);
+    return state.merge({ ...state, locations });
+  };
+  
+  const clearRequest = (state, action) =>
+    state.merge({ ...state, ...initialState });
+  
+  export const reducer = createReducer(initialState, {
+      [KaryabibaranTypes.FETCHALLSAMAJIKKARYABIBARAN_REQUEST]: fetchallsamajikkaryabibaranRequest,
+      [KaryabibaranTypes.FETCHALLSAMAJIKKARYABIBARAN_SUCCESS]: fetchallsamajikkaryabibaranSuccess,
+      [KaryabibaranTypes.FETCHALLSAMAJIKKARYABIBARAN_FAILURE]: fetchallsamajikkaryabibaranFailure,
+
+      [KaryabibaranTypes.LOCATIONS_REQUEST]: locationsRequest,
+      [KaryabibaranTypes.CLEAR_REQUEST]: clearRequest,
+  });
+  
