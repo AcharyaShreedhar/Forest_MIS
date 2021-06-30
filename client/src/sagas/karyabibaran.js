9b9e5cb4d4f8b0f3c668206f5817f524901e8da8
Ashish kc <kecyasis21@gmail.com>
2021-06-30 12:48:05 +0545

    [FM-202] code added for addSamajikkaryabibaran in src/sagas/karyabibaran.js

--- client/src/sagas/karyabibaran.js
+++ client/src/sagas/karyabibaran.js
@@ -1,6 +1,8 @@
-import { put } from "redux-saga/effects";
+import { call,put } from "redux-saga/effects";
 import { isNil } from "ramda";
 import KaryabibaranActions from "../actions/karyabibaran";
+import { toast } from "react-toastify";
+import { history } from "../reducers";
 
 export function* fetchallsamajikkaryabibaranRequest(api, action) {
   const { payload } = action;
@@ -26,5 +28,32 @@ export function* fetchsamajikkaryabibaranRequest(api, action) {
     );
   } else {
     yield put(KaryabibaranActions.fetchsamajikkaryabibaranFailure());
+  }
+}
+
+// Add samajikkaryabibaran
+export function* addsamajikkaryabibaranRequest(api, action) {
+  const { payload } = action;
+
+  const response = yield api.postKaryabibaranSamajikkaryabibaranAddNew(
+    payload.samajikkaryabibaran.data
+  );
+
+  if (response.ok) {
+    toast.success("सफलतापुर्वक सामाजिक कार्य विवरण प्रविष्ट भयो !!!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    yield addsamajikkaryabibaranRequest(api,{
+      name: "post",
+      page: 0,
+      perPage: 10,
+    });
+    yield call(history.push, "/forests/samajikkaryabibaranlist");
+    yield put(KaryabibaranActions.addsamajikkaryabibaranSuccess(response.data));
+  } else {
+    yield put(KaryabibaranActions.addsamajikkaryabibaranFailure());
+    toast.error("तपाईको कार्य सफल हुन सकेन.. कृपया पुनः प्रयास गर्नुहोला !!!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
   }
 }
