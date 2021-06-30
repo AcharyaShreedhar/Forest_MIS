b90f7ffe13cd59cdabb46042b652e7b93084713e
anilakheli <kanxoakheli22@gmail.com>
2021-06-30 16:54:53 +0545

    migration model for sajedari ban

---
+++ server/models/sajedariban_bibaran.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class sajedariban_bibaran extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  sajedariban_bibaran.init({
+    darta_no: DataTypes.STRING,
+    sajedariban_naam: DataTypes.STRING,
+    area: DataTypes.STRING,
+    main_species: DataTypes.STRING,
+    forest_type: DataTypes.STRING,
+    handover_date: DataTypes.STRING,
+    forest_maujdat: DataTypes.STRING,
+    nikasi_timber: DataTypes.STRING,
+    nikasi_wood: DataTypes.STRING,
+    created_by: DataTypes.STRING,
+    updated_by: DataTypes.STRING,
+
+  }, {
+    sequelize,
+    modelName: 'sajedariban_bibaran',
+  });
+  return sajedariban_bibaran;
+};
