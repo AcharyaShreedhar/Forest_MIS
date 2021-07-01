a5676c4393fe1b5de38b78bd05bbfffc1d7a8da4
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-07-01 13:25:05 +0545

    [FM-214]server update sajhedariban_bibarans

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const express = require("express");
 // const jwt = require('jsonwebtoken');
 // var token = require('crypto').randomBytes(50).toString('hex');
-// // 'e6fb380f0a95ffc1d84d09ceb7a5bb2b1283394e356478268c4ac94db6a15a444254369ee1f57eb348d6a8b95e0b763a9800'
+//** */ // 'e6fb380f0a95ffc1d84d09ceb7a5bb2b1283394e356478268c4ac94db6a15a444254369ee1f57eb348d6a8b95e0b763a9800'
 
 const db = require("./app/db");
 var cors = require("cors");
