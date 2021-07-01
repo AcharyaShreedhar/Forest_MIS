726a155503d1c940bb9342ade945a2a54fe07b8b
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-07-01 17:30:18 +0545

    [FM-215]routes update  commercialkabuliyatiban_bibarans

---
+++ server/app/routes/commercialkabuliyatibanBibaranRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+ 
+const commercialkabuliyatibanBibaranController = require("../controller/commercialkabuliyatibanBibaranController");
+router.post(
+  "/commercialkabuliyatibanBibaranList",
+  commercialkabuliyatibanBibaranController.getAllCommercialkabuliyatibanBibaran
+);
+router.get(
+  "/commercialkabuliyatibanBibaran/:commercialkabuliyatibanBibaranId",
+  commercialkabuliyatibanBibaranController.getCommercialkabuliyatibanBibaran
+);
+router.post(
+  "/commercialkabuliyatibanBibaran",
+  commercialkabuliyatibanBibaranController.addCommercialkabuliyatibanBibaran
+);
+router.put(
+  "/commercialkabuliyatibanBibaran/:commercialkabuliyatibanBibaranId",
+  commercialkabuliyatibanBibaranController.updateCommercialkabuliyatibanBibaran
+);
+router.delete(
+  "/commercialkabuliyatibanBibaran/:commercialkabuliyatibanBibaranId",
+  commercialkabuliyatibanBibaranController.deleteCommercialkabuliyatibanBibaran
+);
+module.exports = router;
