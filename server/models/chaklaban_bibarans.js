4bc208a06450bf09d2660d5f289ed7819c0a15cd
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-07-01 13:46:36 +0545

    [FM-214]models update  chaklaban_bibarans

---
+++ server/models/chaklaban_bibarans.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class chaklaban_bibarans extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  chaklaban_bibarans.init({
+    darta_no: DataTypes.STRING,
+    chaklaban_name: DataTypes.STRING,
+    area: DataTypes.STRING,
+    main_species: DataTypes.STRING,
+    forest_type: DataTypes.STRING,
+    handover_date: DataTypes.STRING,
+    forest_maujdat:DataTypes.STRING,
+    nikasi_timber:DataTypes.STRING,
+    nikasi_wood:DataTypes.STRING,
+    created_by: DataTypes.STRING,
+    updated_by: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'chaklaban_bibarans',
+  });
+  return chaklaban_bibarans;
+};
