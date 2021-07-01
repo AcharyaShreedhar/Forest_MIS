fad22fb36deb8e361b05293edebce621db1bcf9e
Ashish kc <kecyasis21@gmail.com>
2021-07-01 16:04:13 +0545

    [FM-219] code added for karmacharibibaran frontend in src/views/Karmachari/Karmacharibibaran/index.jsx

--- client/src/views/Karmachari/Karmacharibibaran/index.jsx
+++ client/src/views/Karmachari/Karmacharibibaran/index.jsx
@@ -1,13 +1,156 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
+import { equals, isNil } from "ramda";
+import { KarmachariBibaran, Filter, ReportGenerator } from "../../../components";
+import KarmacharibibaranActions from "../../../actions/karmacharibibaran";
+import { karmacharibibaranHeadings } from "../../../services/config";
+import { Fragment } from "react";
 
 class Karmacharibibaran extends Component {
-  render() {
-    return (
-      <div>
-       <h1> Karmacharbibaran </h1>
-      </div>
-    )
-  }
-}
+  constructor(props) {
+      super(props);
+      this.state = { loc: "karmacharibibaranlist",perPage: 10, page: 0  };
+      this.handleSelectMenu = this.handleSelectMenu.bind(this);
+      this.handleAdd = this.handleAdd.bind(this);
+      this.handlePageChange = this.handlePageChange.bind(this);
+      this.handlePer = this.handlePer.bind(this);
+      this.fetchResults = this.fetchResults.bind(this);
+    }
+  
+    static getDerivedStateFromProps(nextProps, prevState) {
+      const loc = nextProps.location.pathname.split("/")[2];
+      var karmacharibibaranList = [];
+       if (nextProps !== prevState) {
+          karmacharibibaranList = nextProps.karmacharibibaranDataList.data;
+       } 
+   return { loc,karmacharibibaranList};
+    }
+    handlePer(e) {
+      this.setState({ perPage: e });
+      this.fetchResults(0, e);
+    }
+  
+    fetchResults(page, perPage) {
+      this.props.fetchallKarmacharibibaran({
+        name: "emp_fname_eng",
+        page: page,
+        perPage,
+      });
+    }
 
-export default Karmacharibibaran;
+    handlePageChange(data) {
+      const { perPage } = this.state;
+      this.setState({ page: data.selected });
+      this.fetchResults(data.selected * perPage, perPage);
+    }
+      
+  handleSelectMenu(event, item, path) {
+    switch (event) {
+      case "edit": {
+        this.props.history.push({
+          pathname: `/karmachari/karmacharibibaranedit/${item.emp_id}`,
+          item,
+        });
+        break;
+      }
+      case "delete": {
+        this.props.deleteKarmacharibibaran(item.emp_id);
+        break;
+      }
+      default:
+        break;
+    }
+  }
+  
+    handleAdd() {
+      this.props.history.push("/karmachari/karmacharibibaranadd/new");
+    }
+
+    render() {  
+      const { loc,perPage,karmacharibibaranList,} = this.state;
+       const { user } = this.props;
+     
+        return (
+          <div>
+            {equals(loc, "karmacharibibaranlist") && (
+              <Fragment>
+            <div className="report-filter">
+              <Filter />
+              <ReportGenerator id="karmacharibibaran" />
+            </div>
+              <KarmachariBibaran.List
+                buttonName="+  कर्मचारी विवरण"
+                title="कर्मचारी विवरण सम्बन्धि विवरण"
+                pageCount={
+                !isNil(karmacharibibaranList)
+                  ? Math.ceil(karmacharibibaranList.total / perPage)
+                  : 10
+              }
+               data={!isNil(karmacharibibaranList) ? karmacharibibaranList.list : []}
+               per={perPage}
+               pers={[10, 25, 50, "all"]}
+               onPer={this.handlePer}
+               user={user}
+               headings={karmacharibibaranHeadings}
+               onAdd={() => this.handleAdd("karmacharibibaran")}
+               onSelect={this.handleSelectMenu}
+               onPageClick={(e) => this.handlePageChange(e, "karmacharibibaran")}
+              />
+             </Fragment> 
+            )}
+            {equals(loc, "karmacharibibaranadd") && (
+              <KarmachariBibaran.Add
+                title="+ कर्मचारी विवरण"
+                user={user}
+                onSelect={this.handleSelectMenu}
+                onSubmit={(e) => this.props.addKarmacharibibaran(e)}
+              />
+            )}
+            {equals(loc, "karmacharibibaranedit") && (
+              <KarmachariBibaran.Edit
+                title="कर्मचारी विवरण पुनः प्रविष्ट"
+                history={this.props.history}
+                user={user}
+                onSelect={this.handleSelectMenu}
+                onUpdate={(e, id) => this.props.Karmacharibibaran(e, id)}
+              />
+            )}
+          </div>
+        );
+      }
+    }
+    
+    Karmacharibibaran.propsTypes = {
+      karmacharibibaranDataList: PropTypes.any,
+    };
+    
+    Karmacharibibaran.defaultProps = {
+      karmacharibibaranDataList: {},
+    };
+    
+    const mapStateToProps = (state) => ({
+      user: state.app.user,
+      karmacharibibaranDataList: state.karmacharibibaran.allemployeesData,
+    
+    });
+    
+    const mapDispatchToProps = (dispatch) => ({
+  
+      fetchallKarmacharibibaran: (payload) =>
+      dispatch(KarmacharibibaranActions.fetchallemployeesRequest(payload)),  
+      addKarmacharibibaran: (payload) =>
+        dispatch(KarmacharibibaranActions.addemployeesRequest(payload)),
+      updateKarmacharibibaran: (payload, employeeId) =>
+        dispatch(
+          KarmacharibibaranActions.updateemployeesRequest(payload, employeeId)
+        ),
+      deleteKarmacharibibaran: (employeeId) =>
+        dispatch(KarmacharibibaranActions.deleteemployeesRequest(employeeId)),
+    });
+    
+    export default connect(mapStateToProps, mapDispatchToProps)(Karmacharibibaran);
+    
+      
+  
+  
