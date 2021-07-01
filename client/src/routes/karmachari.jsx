3e589f2e700143e18f583aa45d6a40d6eb97f7c6
Ashish kc <kecyasis21@gmail.com>
2021-07-01 11:48:38 +0545

    [FM-210] Routes for karmachari added

---
+++ client/src/routes/karmachari.jsx
@@ -0,0 +1,66 @@
+import KarmachariBibaran from "../views/Karmachari/Karmacharibibaran";
+import KarmachariDarbandi from "../views/Karmachari/Karmacharidarbandi";
+
+const karmachariRoutes = [
+    {
+      path: "/karmachari/karmacharibibaranlist",
+      title: "KarmachariBibaran",
+      name: "karmacharibibaran",
+      auth: true,
+      component: KarmachariBibaran,
+    },
+  
+    {
+      path: "/karmachari/karmacharibibaranadd/new",
+      title: "KarmachariBibaran",
+      name: "karmacharibibaran",
+      auth: true,
+      component: KarmachariBibaran,
+    },
+  
+    {
+      path: "karmachari/karmacharibibaranedit/:id",
+      title: "KarmachariBibaran",
+      name: "karmacharibibaran",
+      auth: true,
+      component: KarmachariBibaran,
+    },
+  
+  
+    {
+      path: "/karmachari/karmacharidarbandilist",
+      title:"KarmachariDarbandi",
+      name: "karmacharidarbandi",
+      auth: true,
+      component: KarmachariDarbandi,
+    },
+  
+   
+    {
+      path: "/karmachari/karmacharidarbandiadd/new",
+      title: "KarmachariDarbandi",
+      name: "karmacharidarbandi",
+      auth: true,
+      component: KarmachariDarbandi,
+    },
+  
+    {
+      path: "//karmachari/karmacharidarbandiedit/:id",
+      title: "KarmachariDarbandi",
+      name: "karmacharidarbandi",
+      auth: true,
+      component: KarmachariDarbandi,
+    },
+    
+    {
+      redirect: true,
+      path: "/karmachari",
+      to: "/karmachari/karmacharibibaranlist",
+      name: "karmacharibibaran",
+      component: KarmachariBibaran,
+    },
+   
+  ];
+  
+  export default karmachariRoutes;
+  
