7152a8e7588f4a705a3c0f9f25f1d3cb8bef3f2b
anilakheli <kanxoakheli22@gmail.com>
2021-07-02 21:57:48 +0545

    api for rastriyabanbibaran

---
+++ server/app/routes/rastriyabanBibaranRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const rastriyabanBibaranController = require("../controller/rastriyabanBibaranController");
+router.post(
+  "/rastriyabanBibaranList",
+  rastriyabanBibaranController.getAllRastriyabanBibaran
+);
+router.get(
+  "/rastriyabanBibaran/:rastriyabanBibaranId",
+  rastriyabanBibaranController.getRastriyabanBibaran
+);
+router.post(
+  "/rastriyabanBibaran",
+  rastriyabanBibaranController.addRastriyabanBibaran
+);
+router.put(
+  "/rastriyabanBibaran/:rastriyabanBibaranId",
+  rastriyabanBibaranController.updateRastriyabanBibaran
+);
+router.delete(
+  "/rastriyabanBibaran/:rastriyabanBibaranId",
+  rastriyabanBibaranController.deleteRastriyabanBibaran
+);
+module.exports = router;
