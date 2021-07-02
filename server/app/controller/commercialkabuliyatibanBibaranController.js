f26e145ef9a2386892e0a07e0cfef35744aaab40
laxmiprasad316 <laxmiprasad.bastola@gmail.com>
2021-07-02 18:19:23 +0545

    [FM-216]controller update1 commercialkabuliyatiban

--- server/app/controller/commercialkabuliyatibanBibaranController.js
+++ server/app/controller/commercialkabuliyatibanBibaranController.js
@@ -1,34 +1,45 @@
 const pool = require("../db");
-
-//Controller for Listing allCommercialkabuliyatibanBibaran
+//Controller for Listing all CommercialkabuliyatibanBibaran
 async function getAllCommercialkabuliyatibanBibaran(req, res) {
-  const getTotalQuery = "SELECT count(*) as total from commercialkabuliyatiban_bibarans";
-  const getAllCommercialkabuliyatibanBibaranQuery = `select * from commercialkabuliyatiban_bibarans ORDER BY ? ASC LIMIT ?,?`;
-  pool.query(getTotalQuery, [], (error, countresults, fields) => {
-    if (error) throw error;
-    pool.query(
-      getAllCommercialkabuliyatibanBibaranQuery,
-      [req.body.name, req.body.page, req.body.perPage],
-      (error, results, fields) => {
-        if (error) throw error;
-        res.send(
-          JSON.stringify({
-            status: 200,
-            error: null,
-            data: {
-              total: countresults[0].total,
-              list: results,
-            },
-          })
-        );
-      }
-    );
-  });
+  const getTotalQuery =
+    "SELECT count(*) as total from commercialkabuliyatiban_bibarans as n where n.darta_miti BETWEEN ? and ? and n.dist_id like ?";
+  const getAllCommercialkabuliyatibanBibaranQuery = `select * from commercialkabuliyatiban_bibarans as n where n.darta_miti BETWEEN ? and ? and n.dist_id like ? ORDER BY ? DESC LIMIT ?, ?`;
+  pool.query(
+    getTotalQuery,
+    [req.body.fromDate, req.body.toDate, req.body.distId],
+    (error, countresults, fields) => {
+      if (error) throw error;
+      pool.query(
+        getAllCommercialkabuliyatibanBibaranQuery,
+        [
+          req.body.fromDate,
+          req.body.toDate,
+          req.body.distId,
+          req.body.name,
+          req.body.page,
+          req.body.perPage,
+        ],
+        (error, results, fields) => {
+          if (error) throw error;
+          res.send(
+            JSON.stringify({
+              status: 200,
+              error: null,
+              data: {
+                total: countresults[0].total,
+                list: results,
+              },
+            })
+          );
+        }
+      );
+    }
+  );
 }
 
-//Controller for Listing a Commercialkabuliyatiban_Bibabran
+//Controller for Listing a CommercialkabuliyatibanBibaran
 async function getCommercialkabuliyatibanBibaran(req, res) {
-  const getCommercialkabuliyatibanBibaranQuery = `select * from commercialkabuliyatiban_bibarans where commercialkabuliyatiban_bibaran_id=?`;
+  const getCommercialkabuliyatibanBibaranQuery = `select * from commercialkabuliyatiban_bibarans where commercialkabuliyatiban_id=?`;
   pool.query(
     getCommercialkabuliyatibanBibaranQuery,
     [req.params.commercialkabuliyatibanBibaranId],
@@ -41,24 +52,26 @@ async function getCommercialkabuliyatibanBibaran(req, res) {
 
 //Controller for adding a CommercialkabuliyatibanBibaran
 async function addCommercialkabuliyatibanBibaran(req, res) {
-  const addCommercialkabuliyatibanBibaranQuery = `INSERT INTO commercialkabuliyatiban_bibarans (darta_no, commercialkabuliyatiban_name, area, main_species, forest_type, handover_date, forest_maujdat, nikasi_timber, nikasi_wood, created_by, updated_by) values (?,?,?,?,?,?,?,?,?,?,?)`;
+  const addCommercialkabuliyatibanBibaranQuery = `INSERT INTO commercialkabuliyatiban_bibarans (darta_no, darta_miti, dist_id, sajhedariban_naam, address, area, main_species, ghardhuri, lav_jana, created_by, updated_by) values (?,?,?,?,?,?,?,?,?,?,?)`;
   pool.query(
     addCommercialkabuliyatibanBibaranQuery,
     [
-      req.body.darta_no,
-      req.body.commercialkabuliyatiban_name,
-      req.body.area,
-      req.body.main_species,
-      req.body.forest_type,
-      req.body.handover_date, 
-      req.body.forest_maujdat,
-      req.body.nikasi_timber,
-      req.body.nikasi_wood,
+      req.body.darta_no, 
+      req.body.darta_miti, 
+      req.body.dist_id, 
+      req.body.sajhedariban_naam, 
+      req.body.address, 
+      req.body.area, 
+      req.body.main_species, 
+      req.body.ghardhuri, 
+      req.body.lav_jana, 
       req.body.created_by, 
       req.body.updated_by,
     ],
     (error, results, fields) => {
-      if (error) throw error;
+      if (error) {
+        throw error;
+      }
       res.send(JSON.stringify({ status: 200, error: null, data: results }));
     }
   );
@@ -66,22 +79,22 @@ async function addCommercialkabuliyatibanBibaran(req, res) {
 
 //Controller for updating a CommercialkabuliyatibanBibaran
 async function updateCommercialkabuliyatibanBibaran(req, res) {
-  const updateCommercialkabuliyatibanBibaranQuery = `UPDATE commercialkabuliyatiban_bibarans SET darta_no=?, commercialkabuliyatiban_name=?, area=?, main_species=?, forest_type=?, handover_date=?, forest_maujdat=?, nikasi_timber=?, nikasi_wood=?, created_by=?, updated_by=? WHERE commercialkabuliyatiban_bibaran_id=? `;
+  const updateCommercialkabuliyatibanBibaranQuery = `UPDATE commercialkabuliyatiban_bibarans SET darta_no=?, darta_miti=?, dist_id=?, commercialkabuliyatiban_naam=?, address=?, area=?, main_species=?, ghardhuri=?, lav_jana=?, created_by=?, updated_by=? WHERE commercialkabuliyatiban_id=?`;
   pool.query(
     updateCommercialkabuliyatibanBibaranQuery,
     [
-      req.body.darta_no,
-      req.body.commercialkabuliyatiban_name,
-      req.body.area,
-      req.body.main_species,
-      req.body.forest_type,
-      req.body.handover_date, 
-      req.body.forest_maujdat,
-      req.body.nikasi_timber,
-      req.body.nikasi_wood,
-      req.body.created_by, 
-      req.body.updated_by,
-      req.params.commercialkabuliyatibanBibaranId,
+        req.body.darta_no, 
+        req.body.darta_miti,
+        req.body.dist_id, 
+        req.body.sajhedariban_naam, 
+        req.body.address, 
+        req.body.area, 
+        req.body.main_species, 
+        req.body.ghardhuri, 
+        req.body.lav_jana, 
+        req.body.created_by, 
+        req.body.updated_by,
+        req.params.commercialkabuliyatibanBibaranId,
     ],
     (error, results, fields) => {
       if (error) {
@@ -94,7 +107,7 @@ async function updateCommercialkabuliyatibanBibaran(req, res) {
 
 //Controller for deleting a CommercialkabuliyatibanBibaran
 async function deleteCommercialkabuliyatibanBibaran(req, res) {
-  const deleteCommercialkabuliyatibanBibaranQuery = `DELETE  FROM commercialkabuliyatiban_bibarans where commercialkabuliyatiban_bibaran_id=?`;
+  const deleteCommercialkabuliyatibanBibaranQuery = `DELETE  FROM commercialkabuliyatiban_bibarans where commercialkabuliyatiban_id=?`;
   pool.query(
     deleteCommercialkabuliyatibanBibaranQuery,
     [req.params.commercialkabuliyatibanBibaranId],
