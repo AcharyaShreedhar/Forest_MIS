22878cd0fc6efa645eac4e5d1a31128160a15993
Shreedhar Acharya <spacharya2537@gmail.com>
2021-07-03 17:11:30 +0545

    [FM-218]report generation for biruwautpadan and yearlyactivities info

--- client/src/views/Activities/Nursery/index.jsx
+++ client/src/views/Activities/Nursery/index.jsx
@@ -1,19 +1,31 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { PropTypes } from "prop-types";
 import { connect } from "react-redux";
 import { equals, isNil } from "ramda";
-import { BiruwaUtpadan } from "../../../components";
+import { BiruwaUtpadan, Filter, ReportGenerator } from "../../../components";
 import BiruwautpadanActions from "../../../actions/biruwautpadan";
-import { biruwautpadanHeadings } from "../../../services/config";
+import { biruwautpadanHeadings, districtList } from "../../../services/config";
 
 import "./Nursery.scss";
 class Nursery extends Component {
   constructor(props) {
     super(props);
-    this.state = { loc: "biruwautpadanlist", perPage: 10, page: 1 };
+    this.state = {
+      loc: "biruwautpadanlist",
+      fromDate: "2075-01-01",
+      toDate: "2090-12-30",
+      distId: "%",
+      perPage: 10,
+      page: 1,
+    };
     this.handleSelectMenu = this.handleSelectMenu.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
+    this.handleDistrict = this.handleDistrict.bind(this);
+    this.handleToDate = this.handleToDate.bind(this);
+    this.handleFromDate = this.handleFromDate.bind(this);
     this.handlePageChange = this.handlePageChange.bind(this);
+    this.handlePer = this.handlePer.bind(this);
+    this.fetchResults = this.fetchResults.bind(this);
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
@@ -24,16 +36,49 @@ class Nursery extends Component {
     }
     return { biruwautpadanList, loc };
   }
+  handlePer(e) {
+    const { fromDate, toDate, distId } = this.state;
+    this.setState({ perPage: e });
+    this.fetchResults(fromDate, toDate, distId, 0, e);
+  }
+  handleFromDate(e) {
+    const { distId, perPage, toDate } = this.state;
+    this.setState({ fromDate: e });
+    this.fetchResults(e, toDate, distId, 0, perPage);
+  }
+  handleToDate(e) {
+    const { distId, fromDate, perPage } = this.state;
+    this.setState({ toDate: e });
+    this.fetchResults(fromDate, e, distId, 0, perPage);
+  }
+  handleDistrict(e) {
+    const { fromDate, perPage, toDate } = this.state;
+    this.setState({ distId: e });
+    this.fetchResults(fromDate, toDate, e, 0, perPage);
+  }
 
-  handlePageChange(data) {
-    const { perPage } = this.state;
-    this.setState({ page: data.selected });
-
+  fetchResults(fromDate, toDate, distId, page, perPage) {
     this.props.fetchallBiruwautpadan({
+      fromDate,
+      toDate,
+      distId,
       name: "arthik_barsa",
-      page: data.selected * perPage,
+      page: page,
       perPage,
     });
+  }
+
+  handlePageChange(data) {
+    const { fromDate, toDate, distId, perPage } = this.state;
+
+    this.setState({ page: data.selected });
+    this.fetchResults(
+      fromDate,
+      toDate,
+      distId,
+      data.selected * perPage,
+      perPage
+    );
   }
 
   handleSelectMenu(event, item, path) {
@@ -65,21 +110,37 @@ class Nursery extends Component {
     return (
       <div>
         {equals(loc, "nurserylist") && (
-          <BiruwaUtpadan.List
-            buttonName="+ विरुवा उत्पादन"
-            title="विरुवा उत्पादन सम्बन्धी विवरण"
-            pageCount={
-              !isNil(biruwautpadanList)
-                ? Math.ceil(biruwautpadanList.total / perPage)
-                : 10
-            }
-            data={!isNil(biruwautpadanList) ? biruwautpadanList.list : []}
-            user={user}
-            headings={biruwautpadanHeadings}
-            onAdd={() => this.handleAdd()}
-            onSelect={this.handleSelectMenu}
-            onPageClick={(e) => this.handlePageChange(e)}
-          />
+          <Fragment>
+            <div className="report-filter">
+              <Filter
+                id="nursery"
+                title="आर्थिक वर्ष"
+                districtsList={districtList}
+                onToDate={this.handleToDate}
+                onFromDate={this.handleFromDate}
+                onSelect={this.handleDistrict}
+              />
+              <ReportGenerator id="nursery" />
+            </div>
+            <BiruwaUtpadan.List
+              buttonName="+ विरुवा उत्पादन"
+              title="विरुवा उत्पादन सम्बन्धी विवरण"
+              pageCount={
+                !isNil(biruwautpadanList)
+                  ? Math.ceil(biruwautpadanList.total / perPage)
+                  : 10
+              }
+              data={!isNil(biruwautpadanList) ? biruwautpadanList.list : []}
+              per={perPage}
+              pers={[10, 25, 50, "all"]}
+              onPer={this.handlePer}
+              user={user}
+              headings={biruwautpadanHeadings}
+              onAdd={() => this.handleAdd()}
+              onSelect={this.handleSelectMenu}
+              onPageClick={(e) => this.handlePageChange(e)}
+            />
+          </Fragment>
         )}
         {equals(loc, "biruwautpadanadd") && (
           <BiruwaUtpadan.Add
