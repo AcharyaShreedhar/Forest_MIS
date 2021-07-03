0d1b0f9240399452386ee2cf15c802c158fdd97b
anilakheli <kanxoakheli22@gmail.com>
2021-07-03 17:24:12 +0545

    adding routes file

---
+++ server/app/routes/chaklabanBibaranRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const chaklabanBibaranController = require("../controller/chaklabanBibaranController");
+router.post(
+  "/chaklabanBibaranList",
+  chaklabanBibaranController.getAllChaklabanBibaran
+);
+router.get(
+  "/chaklabanBibaran/:chaklabanBibaranId",
+  chaklabanBibaranController.getChaklabanBibaran
+);
+router.post(
+  "/chaklabanBibaran",
+  chaklabanBibaranController.addChaklabanBibaran
+);
+router.put(
+  "/chaklabanBibaran/:chaklabanBibaranId",
+  chaklabanBibaranController.updateChaklabanBibaran
+);
+router.delete(
+  "/chaklabanBibaran/:chaklabanBibaranId",
+  chaklabanBibaranController.deleteChaklabanBibaran
+);
+module.exports = router;
