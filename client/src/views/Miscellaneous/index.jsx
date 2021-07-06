c955ad03f49ded40d9ec72078a1e12a9ad844ca1
Ashish kc <kecyasis21@gmail.com>
2021-07-06 13:28:45 +0545

    [FM-244] Folder added for rojgarsrijana

---
+++ client/src/views/Miscellaneous/index.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { isEmpty } from "ramda";
+import { NotFound } from "../../components";
+import MiscellaneousRoutes from "../../routes/miscellaneous";
+import MiscellaneousActions from "../../actions/miscellaneous";
+
+export class Miscellaneous extends Component {
+    componentDidMount() {
+        this.props.fetchallRojgarsrijana({
+          distId: "%",
+          name: "banka_prakar",
+          page: 0,
+          perPage: 10,
+        });
+      }
+ 
+
+  render() {
+    const { authenticated } = this.props;
+    return (
+      <Switch>
+        {MiscellaneousRoutes.map((prop, key) => {
+          if (prop.redirect && authenticated) {
+            return <Redirect exact from={prop.path} to={prop.to} key={key} />;
+          }
+          if (prop.redirect && !authenticated) {
+            return (
+              <Route
+                exact
+                path={prop.path}
+                component={prop.component}
+                key={key}
+              />
+            );
+          }
+          if (!prop.redirect && prop.auth && !authenticated) {
+            return <Redirect exact from={prop.path} to="/" key={key} />;
+          }
+          return (
+            <Route
+              exact
+              path={prop.path}
+              component={prop.component}
+              key={key}
+            />
+          );
+        })}
+        <Route path="*" exact component={NotFound} />
+      </Switch>
+    );
+  }
+}
+
+Miscellaneous.propTypes = {
+  authenticated: PropTypes.bool.isRequired,
+  history: PropTypes.any,
+};
+
+Miscellaneous.defaultProps = {
+  authenticated: false,
+  history: () => {},
+};
+
+const mapStateToProps = (state) => ({
+  authenticated: !isEmpty(state.app.token),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    fetchallRojgarsrijana: (payload) =>
+    dispatch(MiscellaneousActions.fetchallrojgarsrijanaRequest(payload)), 
+  
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Miscellaneous);
