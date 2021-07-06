b9e8bf9ee6bcec3d4dcc3cb11c807e3a9564e9a0
anilakheli <kanxoakheli22@gmail.com>
2021-07-06 12:58:06 +0545

    adding migration model for pahero_bibaran

---
+++ server/models/pahiro_bibaran.js
@@ -0,0 +1,32 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class pahiro_bibaran extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  pahiro_bibaran.init(
+    {
+      pahiro_gayeko_sthan: DataTypes.STRING,
+      dist_id: DataTypes.INTEGER,
+      manab_ghaite: DataTypes.STRING,
+      manab_mareko: DataTypes.STRING,
+      uddar_sankhya: DataTypes.STRING,
+      pahero_gayeko_miti: DataTypes.STRING,
+      xeti_sankhya: DataTypes.STRING,
+      created_by: DataTypes.STRING,
+      updated_by: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "pahiro_bibaran",
+    }
+  );
+  return pahiro_bibaran;
+};
