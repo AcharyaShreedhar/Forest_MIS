06e8e44d33ae0994f12cea0711b556a9a5e3920f
Ashish kc <kecyasis21@gmail.com>
2021-07-07 13:54:48 +0545

    [FM-248] Conformation function for add samudayikban

--- client/src/components/SamudayikbanBibaran/Add.jsx
+++ client/src/components/SamudayikbanBibaran/Add.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Button, Input, Dropdown } from "../../components";
+import { Button, Input, Dropdown, ConfirmationDialoge } from "../../components";
 import { NepaliDatePicker } from "nepali-datepicker-reactjs";
 import "nepali-datepicker-reactjs/dist/index.css";
 import "./SamudayikbanBibaran.scss";
@@ -30,11 +30,22 @@ class Add extends Component {
       dist_id: "",
       created_by: "",
       updated_by: "",
+      showDialog: false,
+    
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleConfirm = this.handleConfirm.bind(this);
+    this.handleClose = this.handleClose.bind(this);
     this.handleDate = this.handleDate.bind(this);
     this.handleForestType = this.handleForestType.bind(this);
+  }
+
+  handleConfirm() {
+    this.setState({ showDialog: !this.state.showDialog });
+  }
+  handleClose() {
+    this.setState({ showDialog: !this.state.showDialog });
   }
 
   handleSubmit() {
@@ -117,11 +128,21 @@ class Add extends Component {
       renewed_date,
       nabikaran_abadhi,
       renewal_date,
+      showDialog,
     } = this.state;
 
     return (
       <React.Fragment>
         <div className=" card p-5 border-5">
+        <ConfirmationDialoge
+            showDialog={showDialog}
+            title="थप"
+            body="के तपाईँ सामुदायिक वन थप गर्न चाहनुहुन्छ ?"
+            confirmLabel="चाहन्छु "
+            cancelLabel="चाहंदिन "
+            onYes={this.handleSubmit}
+            onClose={this.handleClose}
+          />
           <div className="detail-content">
             <div className="title">
               <span className="dsl-b22">{title}</span>
@@ -227,7 +248,7 @@ class Add extends Component {
               <Button
                 className="mr-3"
                 name="Save"
-                onClick={this.handleSubmit.bind(this)}
+                onClick={this.handleConfirm.bind(this)}
               />
             </div>
           </div>
@@ -239,10 +260,12 @@ class Add extends Component {
 
 Add.propTypes = {
   regno: PropTypes.number,
+  onClose: PropTypes.func,
 };
 
 Add.defaultProps = {
   regno: 1,
+  onClose: () => {},
 };
 
 export default Add;
