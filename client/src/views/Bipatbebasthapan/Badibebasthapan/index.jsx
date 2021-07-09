c88c00eb461f39503cf9efe7306baaadb3f9e256
Subash Paudel <subashpaudel144@gmail.com>
2021-07-09 12:21:13 +0545

    [FM-249] Frontend for list badibebasthapan

--- client/src/views/Bipatbebasthapan/Badibebasthapan/index.jsx
+++ client/src/views/Bipatbebasthapan/Badibebasthapan/index.jsx
@@ -1,11 +1,206 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from "react";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
+import { equals, isNil } from "ramda";
+import { BadiBebasthapan, Filter, ReportGenerator } from "../../../components";
+import BipatbibaranActions from "../../../actions/bipatbibaran";
+import { badibebasthapanHeadings, districtList } from "../../../services/config";
 
-export class Badibebasthapan extends Component {
-    render() {
-        return (
-            <h1>Badibebasthapan</h1>
-        )
+class Badibebasthapan extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loc: "badibebasthapanlist",
+      fromDate: "2075-01-01",
+      toDate: "2090-12-30",
+      distId: "%",
+      perPage: 10,
+      page: 1,
+    };
+    this.handleSelectMenu = this.handleSelectMenu.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleDistrict = this.handleDistrict.bind(this);
+    this.handleToDate = this.handleToDate.bind(this);
+    this.handleFromDate = this.handleFromDate.bind(this);
+    this.handlePageChange = this.handlePageChange.bind(this);
+    this.handlePer = this.handlePer.bind(this);
+    this.fetchResults = this.fetchResults.bind(this);
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    const loc = nextProps.location.pathname.split("/")[2];
+    var badibebasthapanList = [];
+    if (nextProps !== prevState) {
+      badibebasthapanList = nextProps.badibebasthapanDataList.data;
     }
+
+    return {
+      loc,
+      badibebasthapanList,
+    };
+  }
+  handlePer(e) {
+    const { fromDate, toDate, distId } = this.state;
+    this.setState({ perPage: e });
+    this.fetchResults(fromDate, toDate, distId, 0, e);
+  }
+  handleFromDate(e) {
+    const { distId, perPage, toDate } = this.state;
+    this.setState({ fromDate: e });
+    this.fetchResults(e, toDate, distId, 0, perPage);
+  }
+  handleToDate(e) {
+    const { distId, fromDate, perPage } = this.state;
+    this.setState({ toDate: e });
+    this.fetchResults(fromDate, e, distId, 0, perPage);
+  }
+  handleDistrict(e) {
+    const { fromDate, perPage, toDate } = this.state;
+    this.setState({ distId: e });
+    this.fetchResults(fromDate, toDate, e, 0, perPage);
+  }
+
+  fetchResults(fromDate, toDate, distId, page, perPage) {
+    this.props.fetchallBadibebasthapan({
+      fromDate,
+      toDate,
+      distId,
+      name: "badhi_aayeko_miti",
+      page: page,
+      perPage,
+    });
+  }
+
+  handlePageChange(data) {
+    const { fromDate, toDate, distId, perPage } = this.state;
+    this.setState({ page: data.selected });
+    this.fetchResults(
+      fromDate,
+      toDate,
+      distId,
+      data.selected * perPage,
+      perPage
+    );
+  }
+
+  handleSelectMenu(event, item, path) {
+    switch (event) {
+      case "edit": {
+        this.props.history.push({
+          pathname: `/bipatbebasthapan/badibebasthapanedit/${item.badhi_bibaran_id}`,
+          item,
+        });
+
+        break;
+      }
+      case "delete": {
+        this.props.deleteBadibebasthapan(item.badi_bibaran_id);
+        break;
+      }
+      default:
+        break;
+    }
+  }
+
+  handleAdd() {
+    this.props.history.push("/bipatbebasthapan/badibebasthapanadd/new");
+  }
+
+  render() {
+    const { loc, perPage, badibebasthapanList } = this.state;
+    const { user } = this.props;
+
+    return (
+      <div>
+        {equals(loc, "badibebasthapanlist") && (
+          <Fragment>
+            <div className="report-filter">
+              <Filter
+                id="badibebasthapan"
+                title="बाढी आएको मिति"
+                districtsList={districtList}
+                onToDate={this.handleToDate}
+                onFromDate={this.handleFromDate}
+                onSelect={this.handleDistrict}
+              />
+              <ReportGenerator id="badibebasthapan" />
+            </div>
+            <BadiBebasthapan.List
+              buttonName="+ बाढी व्यवस्थापन"
+              title="बाढी व्यवस्थापन सम्बन्धि विवरण"
+              pageCount={
+                !isNil(badibebasthapanList)
+                  ? Math.ceil(badibebasthapanList.total / perPage)
+                  : 10
+              }
+              data={
+                !isNil(badibebasthapanList) ? badibebasthapanList.list : []
+              }
+              per={perPage}
+              pers={[10, 25, 50, "all"]}
+              onPer={this.handlePer}
+              headings={badibebasthapanHeadings}
+              user={user}
+              onAdd={() => this.handleAdd("badibebasthapan")}
+              onSelect={this.handleSelectMenu}
+              onPageClick={(e) => this.handlePageChange(e)}
+            />
+          </Fragment>
+        )}
+        {equals(loc, "badibebasthapanadd") && (
+          <BadiBebasthapan.Add
+            title="+ बाढी व्यवस्थापन"
+            user={user}
+            onSelect={this.handleSelectMenu}
+            onSubmit={(e) => this.props.addBadibebasthapan(e)}
+          />
+        )}
+        {equals(loc, "badibebasthapanedit") && (
+          <BadiBebasthapan.Edit
+            title="बाढी व्यवस्थापन शंसोधन"
+            user={user}
+            history={this.props.history}
+            onSelect={this.handleSelectMenu}
+            onUpdate={(e, id) => this.props.updateBadibebasthapan(e, id)}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
-export default Badibebasthapan;
+Badibebasthapan.propsTypes = {
+  badibeasthapanDataList: PropTypes.any,
+};
+
+Badibebasthapan.defaultProps = {
+  badibebasthapanDataList: {},
+};
+
+const mapStateToProps = (state) => ({
+  user: state.app.user,
+  badibebasthapanDataList: state.bipatbibaran.allbadhibibaranData,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchallBadibebasthapan: (payload) =>
+    dispatch(BipatbibaranActions.fetchallbadhibibaranRequest(payload)),
+
+  addBadibebasthapan: (payload) =>
+    dispatch(BipatbibaranActions.addbadhibibaranRequest(payload)),
+
+  updateBadibebasthapan: (payload, badibibaranId) =>
+    dispatch(
+        BipatbibaranActions.updatebadhibibaranRequest(
+        payload,
+        badibibaranId
+      )
+    ),
+
+  deleteBadibebasthapan: (badibibaranId) =>
+    dispatch(
+        BipatbibaranActions.deletebadhibibaranRequest(badibibaranId)
+    ),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Badibebasthapan);
