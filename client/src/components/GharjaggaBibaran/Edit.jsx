d18c96b50bd0fdb147f24310eba9db3341ad4a6e
Ashish kc <kecyasis21@gmail.com>
2021-07-09 17:51:31 +0545

    [FM-248] Confirmation functionality for gharjgga

--- client/src/components/GharjaggaBibaran/Edit.jsx
+++ client/src/components/GharjaggaBibaran/Edit.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Input, Dropdown } from "../../components";
+import { Button, Input, Dropdown, ConfirmationDialoge } from "../../components";
 import { equals } from "ramda";
 
 const assetsType = [
@@ -22,10 +22,20 @@ class Edit extends Component {
       dist_id: props.history.location.item.dist_id,
       created_by: props.history.location.item.created_by,
       updated_by: props.history.location.item.updated_by,
+      showDialog: false,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleAssetType = this.handleAssetType.bind(this);
+    this.handleConfirm = this.handleConfirm.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleConfirm() {
+    this.setState({ showDialog: !this.state.showDialog });
+  }
+  handleClose() {
+    this.setState({ showDialog: !this.state.showDialog });
   }
 
   handleSubmit() {
@@ -39,7 +49,6 @@ class Edit extends Component {
       unit,
       remarks,
       created_by,
-      updated_by,
     } = this.state;
     const payload = {
       assets: {
@@ -73,11 +82,21 @@ class Edit extends Component {
       land_area,
       unit,
       remarks,
+      showDialog,
     } = this.state;
 
     return (
       <React.Fragment>
         <div className=" card p-5 border-5">
+        <ConfirmationDialoge
+            showDialog={showDialog}
+            title="शंसोधन"
+            body="के तपाईँ घर जग्गा सम्बन्धी विवरण शंसोधन गर्न चाहनुहुन्छ ?"
+            confirmLabel="चाहन्छु "
+            cancelLabel="चाहंदिन "
+            onYes={this.handleSubmit}
+            onClose={this.handleClose}
+          />
           <div className="detail-content">
             <div className="title">
               <span className="dsl-b22">{title}</span>
@@ -148,7 +167,7 @@ class Edit extends Component {
               <Button
                 className="mr-3"
                 name="Update"
-                onClick={this.handleSubmit.bind(this)}
+                onClick={this.handleConfirm.bind(this)}
               />
             </div>
           </div>
