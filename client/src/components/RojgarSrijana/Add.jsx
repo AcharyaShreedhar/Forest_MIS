a719cb068dfd3a0623077632f96ead9a773b8f94
Ashish kc <kecyasis21@gmail.com>
2021-07-11 14:02:31 +0545

    [FM-248] Confirmation functionality for rojgarsrijana

--- client/src/components/RojgarSrijana/Add.jsx
+++ client/src/components/RojgarSrijana/Add.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { Button, Input, Dropdown } from "../../components";
-import { equals } from "ramda";
+import { Button, Input, ConfirmationDialoge } from "../../components";
 
 
 class Add extends Component {
@@ -17,9 +16,19 @@ class Add extends Component {
         dist_id: "",
         created_by: "",
         updated_by: "",
+        showDialog: false,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleConfirm = this.handleConfirm.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleConfirm() {
+    this.setState({ showDialog: !this.state.showDialog });
+  }
+  handleClose() {
+    this.setState({ showDialog: !this.state.showDialog });
   }
 
   handleSubmit() {
@@ -61,11 +70,21 @@ class Add extends Component {
         purus,
         jamma,
         kaifiyat,
+        showDialog,
     } = this.state;
 
     return (
       <React.Fragment>
         <div className=" card p-5 border-5">
+        <ConfirmationDialoge
+            showDialog={showDialog}
+            title="थप"
+            body="के तपाईँ रोजगार सिर्जना सम्बन्धि विवरण थप गर्न चाहनुहुन्छ ?"
+            confirmLabel="चाहन्छु "
+            cancelLabel="चाहंदिन "
+            onYes={this.handleSubmit}
+            onClose={this.handleClose}
+          />
           <div className="detail-content">
             <div className="title">
               <span className="dsl-b22">{title}</span>
@@ -128,7 +147,7 @@ class Add extends Component {
               <Button
                 className="mr-3"
                 name="Save"
-                onClick={this.handleSubmit.bind(this)}
+                onClick={this.handleConfirm.bind(this)}
               />
             </div>
           </div>
