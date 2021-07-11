663c9d4f43b69c7dfb32b494c8321c3c8d2100e4
Ashish kc <kecyasis21@gmail.com>
2021-07-11 13:54:17 +0545

    [FM-248] Confirmation functionality for samajikkarya bibaran

--- client/src/components/SamajikKaryabibaran/Edit.jsx
+++ client/src/components/SamajikKaryabibaran/Edit.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { Button, Input, Dropdown } from "../../components";
-import { equals } from "ramda";
+import { Button, Input, ConfirmationDialoge } from "../../components";
 
 
 class Edit extends Component {
@@ -16,9 +15,19 @@ class Edit extends Component {
       dist_id: props.history.location.item.dist_id,
       created_by: props.history.location.item.created_by,
       updated_by: props.history.location.item.updated_by,
+      showDialog: false,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleConfirm = this.handleConfirm.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleConfirm() {
+    this.setState({ showDialog: !this.state.showDialog });
+  }
+  handleClose() {
+    this.setState({ showDialog: !this.state.showDialog });
   }
 
   handleSubmit() {
@@ -57,11 +66,21 @@ class Edit extends Component {
         samajik_parinam,
         samajik_bajetkharcha,
         ban_type,
+        showDialog,
     } = this.state;
 
     return (
       <React.Fragment>
         <div className=" card p-5 border-5">
+        <ConfirmationDialoge
+            showDialog={showDialog}
+            title="शंसोधन"
+            body="के तपाईँ सामाजिक कार्य सम्बन्धि विवरण शंसोधन गर्न चाहनुहुन्छ ?"
+            confirmLabel="चाहन्छु "
+            cancelLabel="चाहंदिन "
+            onYes={this.handleSubmit}
+            onClose={this.handleClose}
+          />
           <div className="detail-content">
             <div className="title">
               <span className="dsl-b22">{title}</span>
@@ -111,7 +130,7 @@ class Edit extends Component {
               <Button
                 className="mr-3"
                 name="Save"
-                onClick={this.handleSubmit.bind(this)}
+                onClick={this.handleConfirm.bind(this)}
               />
             </div>
           </div>
