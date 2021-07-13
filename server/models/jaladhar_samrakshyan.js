64bac3c23694e3653e9481a7cc972f04f0f23daf
anilakheli <kanxoakheli22@gmail.com>
2021-07-13 15:48:51 +0545

    [fm-254] migration model for jaladhar samraxan

---
+++ server/models/jaladhar_samrakshyan.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class jaladhar_samrakshyan extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  jaladhar_samrakshyan.init(
+    {
+      dist_id: DataTypes.STRING,
+      sthan: DataTypes.STRING,
+      qty: DataTypes.STRING,
+      karyakram_miti: DataTypes.STRING,
+      laagat: DataTypes.STRING,
+      created_by: DataTypes.STRING,
+      updated_by: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "jaladhar_samrakshyan",
+    }
+  );
+  return jaladhar_samrakshyan;
+};
