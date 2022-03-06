41a85daf471d4fadf34e342ce42c5b29aae15b99
JupiterTamrakar <tamrakarjupiter@gmail.com>
2022-03-06 16:40:42 +0545

    changed kabuliyatiban_nabikaran to dharmikban_nabikaran and removed susasan from report

--- server/app/controller/report/nabikaranbibaranController.js
+++ server/app/controller/report/nabikaranbibaranController.js
@@ -1,25 +1,24 @@
-const pool = require("../../db");
+const pool = require('../../db')
 
 //Controller for Nabikaran Bibaran Data
 
 async function getNabikaranBibaran(req, res) {
-
-  let office_cond = "office_id like ?"
-  const office_len=(Array.isArray(req.body.officeId)) ? req.body.officeId.length : 0
-  if(office_len > 1){
-    office_cond = "office_id in (?)"
+  let office_cond = 'office_id like ?'
+  const office_len = Array.isArray(req.body.officeId)
+    ? req.body.officeId.length
+    : 0
+  if (office_len > 1) {
+    office_cond = 'office_id in (?)'
   }
 
-  let dist_cond = "dist_id like ?"
-  const dist_len=(Array.isArray(req.body.distId)) ? req.body.distId.length : 0
-  if(dist_len > 1){
-    dist_cond = "dist_id in (?)"
+  let dist_cond = 'dist_id like ?'
+  const dist_len = Array.isArray(req.body.distId) ? req.body.distId.length : 0
+  if (dist_len > 1) {
+    dist_cond = 'dist_id in (?)'
   }
 
-  const getSNabikaranBibaranQuery =
-    `SELECT SUM(IF(Year(n.renewal_date)<=?,1,0)) AS expired,SUM(IF(Year(n.renewal_date)=?,1,0)) AS renewed,SUM(IF(Year(n.renewal_date)!=?,1,0)) AS remaining,SUM(IF(Year(n.renewal_date)=?,1,0)) as tobeexpired,SUM(IF(Year(n.renewal_date)!=? OR Year(n.renewal_date)=?,1,0)) as torenew FROM samudayikban_bibarans as s LEFT JOIN nabikaran_karyayojanas as n ON s.darta_no=n.darta_id where s.${dist_cond} and s.${office_cond}`;
-  const getKNabikaranBibaranQuery =
-    `SELECT SUM(IF(Year(n.renewal_date)<=?,1,0)) AS expired,SUM(IF(Year(n.renewal_date)=?,1,0)) AS renewed,SUM(IF(Year(n.renewal_date)!=?,1,0)) AS remaining,SUM(IF(Year(n.renewal_date)=?,1,0)) as tobeexpired,SUM(IF(Year(n.renewal_date)!=? OR Year(n.renewal_date)=?,1,0)) as torenew FROM kabuliyatiban_bibarans as s LEFT JOIN nabikaran_karyayojanas as n ON s.darta_no=n.darta_id where s.${dist_cond} and s.${office_cond}`;
+  const getSNabikaranBibaranQuery = `SELECT SUM(IF(Year(n.renewal_date)<=?,1,0)) AS expired,SUM(IF(Year(n.renewal_date)=?,1,0)) AS renewed,SUM(IF(Year(n.renewal_date)!=?,1,0)) AS remaining,SUM(IF(Year(n.renewal_date)=?,1,0)) as tobeexpired,SUM(IF(Year(n.renewal_date)!=? OR Year(n.renewal_date)=?,1,0)) as torenew FROM samudayikban_bibarans as s LEFT JOIN nabikaran_karyayojanas as n ON s.darta_no=n.darta_id where s.${dist_cond} and s.${office_cond}`
+  const getKNabikaranBibaranQuery = `SELECT SUM(IF(Year(n.renewal_date)<=?,1,0)) AS expired,SUM(IF(Year(n.renewal_date)=?,1,0)) AS renewed,SUM(IF(Year(n.renewal_date)!=?,1,0)) AS remaining,SUM(IF(Year(n.renewal_date)=?,1,0)) as tobeexpired,SUM(IF(Year(n.renewal_date)!=? OR Year(n.renewal_date)=?,1,0)) as torenew FROM dharmikban_bibarans as s LEFT JOIN nabikaran_karyayojanas as n ON s.darta_no=n.darta_id where s.${dist_cond} and s.${office_cond}`
   pool.query(
     getSNabikaranBibaranQuery,
     [
@@ -33,7 +32,7 @@ async function getNabikaranBibaran(req, res) {
       req.body.officeId,
     ],
     (error, sresults, fields) => {
-      if (error) throw error;
+      if (error) throw error
       pool.query(
         getKNabikaranBibaranQuery,
         [
@@ -47,7 +46,7 @@ async function getNabikaranBibaran(req, res) {
           req.body.officeId,
         ],
         (error, kresults, fields) => {
-          if (error) throw error;
+          if (error) throw error
           res.send(
             JSON.stringify({
               status: 200,
@@ -57,13 +56,13 @@ async function getNabikaranBibaran(req, res) {
                 samudayikban: sresults[0],
               },
             })
-          );
+          )
         }
-      );
+      )
     }
-  );
+  )
 }
 
 module.exports = {
   getNabikaranBibaran,
-};
+}
