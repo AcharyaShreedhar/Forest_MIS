1bd13eb7b40b4027b015c8cfeaee794e40313119
JupiterTamrakar <tamrakarjupiter@gmail.com>
2022-03-20 13:01:54 +0545

    [FM-313] added serverside setup for karyasirshaks

---
+++ server/app/controller/karyakramSirshakController.js
@@ -0,0 +1,116 @@
+const pool = require('../db')
+
+//Controller for Listing all karyakram_sirshak
+async function getAllKaryakramSirshak(req, res) {
+  const getTotalQuery = `SELECT count(*) as total from karyakram_sirshaks`
+  const getAllKaryakramSirshakQuery = `select * from karyakram_sirshaks ORDER BY ? ASC LIMIT ?, ?`
+
+  pool.query(getTotalQuery, (error, countresults, fields) => {
+    if (error) throw error
+    pool.query(
+      getAllKaryakramSirshakQuery,
+      [req.body.karyakram_name, req.body.page, req.body.perPage],
+      (error, results, fields) => {
+        if (error) throw error
+        res.send(
+          JSON.stringify({
+            status: 200,
+            error: null,
+            data: {
+              total: countresults[0].total,
+              list: results,
+            },
+          })
+        )
+      }
+    )
+  })
+}
+
+//Controller for Listing a karyakram_sirshak
+async function getKaryakramSirshak(req, res) {
+  const getKaryakramSirshakQuery = `select * from karyakram_sirshaks where karyakram_Shirshak_id = ?`
+  pool.query(
+    getKaryakramSirshakQuery,
+    [req.params.karyakramShirshakId],
+    (error, results, fields) => {
+      if (error) throw error
+      res.send(JSON.stringify({ status: 200, error: null, data: results }))
+    }
+  )
+}
+
+//Controller for adding a Anya_Samapti
+async function addKaryakramSirshak(req, res, next) {
+  const addKaryakramSirshakQuery = `INSERT INTO karyakram_sirshaks (sirshak_id, dist_id, office_id, user_id, karyakram_name, sirshak_no, created_by,updated_by) values (?,?,?,?,?,?,?,?)`
+  pool.query(
+    addKaryakramSirshakQuery,
+    [
+      req.body.sirshak_id,
+      req.body.dist_id,
+      req.body.office_id,
+      req.body.user_id,
+      req.body.karyakram_name,
+      req.body.sirshak_no,
+      req.body.created_by,
+      req.body.updated_by,
+    ],
+    (error, results, fields) => {
+      if (error) {
+        console.log(error)
+        next(error)
+      }
+      res.send(JSON.stringify({ status: 200, error: error, data: results }))
+    }
+  )
+}
+
+//Controller for updating a karyakram_sirshak
+async function updateKaryakramSirshak(req, res, next) {
+  const updateKaryakramSirshakQuery = `UPDATE karyakram_sirshaks SET sirshak_id = ?, dist_id=?, office_id=?, user_id=?, karyakram_name=?, sirshak_no=?, created_by=?,updated_by=? WHERE karyakram_sirshak_id=?`
+  pool.query(
+    updateKaryakramSirshakQuery,
+    [
+      req.body.sirshak_id,
+      req.body.dist_id,
+      req.body.office_id,
+      req.body.user_id,
+      req.body.karyakram_name,
+      req.body.sirshak_no,
+      req.body.created_by,
+      req.body.updated_by,
+      req.params.karyakramSirshakId,
+    ],
+    (error, results, fields) => {
+      if (error) {
+        console.log(error)
+        next(error)
+      }
+      res.send(JSON.stringify({ status: 200, error: error, data: results }))
+    }
+  )
+}
+
+//Controller for deleting a KaryakramSirshak
+async function deleteKaryakramSirshak(req, res, next) {
+  const deleteKaryakramSirshakQuery = `DELETE  FROM karyakram_sirshaks where karyakram_sirshak_id=?`
+  pool.query(
+    deleteKaryakramSirshakQuery,
+    [req.params.karyakramSirshakId],
+    (error, results, fields) => {
+      if (error) {
+        console.log(error)
+        next(error)
+      }
+      res.send(JSON.stringify({ status: 200, error: error, data: results }))
+    }
+  )
+}
+
+module.exports = {
+  getAllKaryakramSirshak,
+  getKaryakramSirshak,
+  addKaryakramSirshak,
+  updateKaryakramSirshak,
+  deleteKaryakramSirshak,
+}
---
+++ server/app/routes/karyakramSirshakRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+
+const karyakramSirshakController = require('../controller/karyakramSirshakController')
+router.post(
+  '/karyakramsirshakList',
+  karyakramSirshakController.getAllKaryakramSirshak
+)
+router.get(
+  '/karyakramsirshak/:karyakramSirshakId',
+  karyakramSirshakController.getKaryakramSirshak
+)
+router.post('/karyakramsirshak', karyakramSirshakController.addKaryakramSirshak)
+router.put(
+  '/karyakramsirshak/:karyakramSirshakId',
+  karyakramSirshakController.updateKaryakramSirshak
+)
+router.delete(
+  '/karyakramsirshak/:karyakramSirshakId',
+  karyakramSirshakController.deleteKaryakramSirshak
+)
+module.exports = router
